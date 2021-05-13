1a6a091287f7923ef4750188549581aa2c902e4e
Parker MacCready <p.maccready@gmail.com>
2021-05-12 17:00:19 -0700

    dot_in
    first development of this code

---
+++ alpha/dot_in_argfun.py
@@ -0,0 +1,51 @@
+"""
+Shared helper functions for the dot_in code, especially for argument passing.
+
+"""
+import argparse
+import sys
+from pathlib import Path
+import Lfun, zfun # path to alpha set by calling function
+
+def intro():
+    parser = argparse.ArgumentParser()
+    # required arguments
+    parser.add_argument('-g', '--gridname', type=str)   # e.g. cas6
+    parser.add_argument('-t', '--tag', type=str)        # e.g. v3
+    parser.add_argument('-x', '--ex_name', type=str)    # e.g. lo8
+    parser.add_argument('-r', '--run_type', type=str)   # backfill or forecast
+    parser.add_argument('-s', '--start_type', type=str) # new or continuation
+    parser.add_argument('-d', '--date_string', type=str) # e.g. 2019.07.04
+    parser.add_argument('-bu', '--blow_ups', type=int) # e.g. 0
+    parser.add_argument('-np', '--np_num', type=int) # e.g. 196, number of cores
+    # optional arguments
+    parser.add_argument('-test', '--testing', default=False, type=zfun.boolean_string)
+    
+    # get the args
+    args = parser.parse_args()
+    
+    # test that required arguments were provided
+    argsd = args.__dict__
+    for a in ['gridname', 'tag', 'ex_name', 'run_type', 'start_type', 'date_string',
+                'blow_ups', 'np_num']:
+        if argsd[a] == None:
+            print('*** Missing required argument to forcing_argfun.intro(): ' + a)
+            sys.exit()
+        
+    # get the dict Ldir
+    Ldir = Lfun.Lstart(gridname=args.gridname, tag=args.tag, ex_name=args.ex_name)
+    # add more entries to Ldir for use by make_dot_in.py
+    for a in ['run_type', 'start_type', 'date_string', 'blow_ups', 'np_num', 'testing']:
+        Ldir[a] = argsd[a]
+        
+    # create the expected output directories if needed
+    # (a convenience when running make_forcing_main.py on its own while testing)
+    # out_dir = Ldir['LOo'] / 'forcing' / Ldir['gtag'] / ('f' + Ldir['date_string']) / Ldir['frc']
+    # Lfun.make_dir(out_dir)
+    # Lfun.make_dir(out_dir / 'Info')
+    # Lfun.make_dir(out_dir / 'Data')
+
+    return Ldir.copy()
+    
+    
+
---
+++ dot_in/README.md
@@ -0,0 +1,7 @@
+# Info for the dot_in code
+
+The purpose of this code is
+
+---
+
+## Subheading
