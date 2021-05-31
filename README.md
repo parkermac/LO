e45f9725511cac7defb4b351e8ff70e544c78a07
Parker MacCready <p.maccready@gmail.com>
2021-05-31 12:38:04 -0700

    moor
    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README for LO
 
-LO is the code base for running the LiveOcean collection of regional ocean simulations.
+## LO is the code base for running the LiveOcean collection of regional ocean simulations.
 
 The code here handles all the non-research tasks of making model (e.g. ROMS) forcing files, running the model on a remote linux cluster, and post-processing of model output.  Many of the post-processing tasks, such as mooring- cast- and layer extractions, and particle tracking, are designed for other users to be able to run from the linux command line, or modify if needed.
 
@@ -28,12 +28,15 @@ On your machine, go to wherever you want the LO repo to end up, and do:
 ```
 git clone https://github.com/parkermac/LO.git
 ```
-
 and LO and all its sub-folders will appear. To get any changes I may make, go to any folder in LO and do:
 ```
 git pull
 ```
-
+You will also need python3 and various modules. Start by installing the latest version of miniconda on your computer, from [Continuum](https://docs.conda.io/en/latest/miniconda.html). Then create an environment that has all the modules required for this code by going to LO/alpha and executing:
+```
+conda env create -f loenv.yml
+```
+You can look in the yml file to see what is being installed.  It even adds the non-python nco toolbox and ffmpeg. For a very few pieces of code you also need matlab, and these are being deprecated, so you can likely ignore this requirement.
 
 ---
 
@@ -47,6 +50,20 @@ These four directories are assumed to be somewhere, all at the same level in the
 - LO_user: is a placeholder that a user should create to house modified version of the LO code.
 
 LO_output is typically made, if needed, by the code that writes to it. LO_user has to be made by hand (more on that below).
+
+A lot of the code makes use of a dictionary "Ldir" that contains Path objects about where things are. This is created in a somewhat complicated way:
+- It is initially specified in `alpha/get_lo_info.py`
+- You don't run `get_lo_info.py` itself, but instead it is run every time you run the method `alpha/Lfun.Lstart()` which adds a few more application-specific entries to Ldir.
+
+In principle the code is designed to accommodate other users by allowing them to make another directory (which they can make as their repo) next to LO, called LO_user where you make `alpha/user_get_lo_info.py`.  There are hooks built into the LO code in strategic places that look for
+user version of modules allowing you to do things like design your own particle tracking experiments and even add new particle behavior.
+
+#### `get_lo_info.py` or `user_get_lo_info.py` are designed to be the one place where you set machine-dependent choices.  It looks to see what machine you are working on.  It allows you to set several paths to model output, for example: Ldir['roms_out'], Ldir['roms_out1'], and so on.
+
+You can see what is in Ldir by running the module from ipython:
+```
+run Lfun
+```
 
 ---
 
---
+++ extract/moor/extract_moor.py
@@ -0,0 +1,135 @@
+"""
+This is code for doing mooring extractions.
+
+Test on mac in ipython:
+
+run extract_moor.py -g cas6 -t v3 -x lo8b -ro 1 -0 2019.07.04 -1 2019.07.06
+
+"""
+
+from pathlib import Path
+import sys
+from datetime import datetime
+
+pth = Path(__file__).absolute().parent.parent.parent / 'alpha'
+if str(pth) not in sys.path:
+    sys.path.append(str(pth))
+import extract_argfun as exfun
+
+Ldir = exfun.intro() # this handles the argument passing
+result_dict = dict()
+result_dict['start_dt'] = datetime.now()
+
+# ****************** CASE-SPECIFIC CODE *****************
+
+import pandas as pd
+import zfun
+import zrfun
+import subprocess
+import netCDF4 as nc
+import Lfun
+import numpy as np
+
+# ls | grep ocean_his | ncrcat -d xi_rho,10 -d eta_rho,10 -d xi_u,10 -d eta_u,10 -d xi_v,10 -d eta_v,10 -O moor.nc
+
+def get_moor(out_fn, fn, lon, lat):
+    
+    # This function does the cast extraction and saves it to a NetCDF file.
+    
+    # Find indicies nearest to the location
+    G, S, T = zrfun.get_basic_info(fn)
+    ix = zfun.find_nearest_ind(G['lon_rho'][0,:], lon)
+    iy = zfun.find_nearest_ind(G['lat_rho'][:,0], lat)
+    
+    # Run ncks to do the extraction, overwriting any existing file
+    cmd_list = ['ncks', '-d', 'xi_rho,'+str(ix), '-d', 'eta_rho,'+str(iy),
+        '-v', 'AKs,salt,temp,NO3,phytoplankton,zooplankton,detritus,Ldetritus,oxygen,alkalinity,TIC,h',
+        '-O', str(fn), str(out_fn)]
+    # Note: 3-D variables will retain singelton dimensions
+    # Note: We get AKs so that the s_w dimension is retained
+    proc = subprocess.Popen(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # and check on the results
+    stdout, stderr = proc.communicate()
+    if len(stdout) > 0:
+        print('\n' + ' sdtout '.center(60,'-'))
+        print(stdout.decode())
+    if len(stderr) > 0:
+        print('\n' + ' stderr '.center(60,'-'))
+        print(stderr.decode())
+    
+    # Add z-coordinates to the file
+    foo = nc.Dataset(out_fn, 'a')
+    z_rho, z_w = zrfun.get_z(foo['h'][:], np.array([0.]), S)
+    vv = foo.createVariable('z_rho', float, ('s_rho',))
+    vv.long_name = 'vertical position on s_rho grid, positive up, zero at surface'
+    vv.units = 'm'
+    vv[:] = z_rho
+    vv = foo.createVariable('z_w', float, ('s_w',))
+    vv.long_name = 'vertical position on s_w grid, positive up, zero at surface'
+    vv.units = 'm'
+    vv[:] = z_w
+    # Note that z_rho and z_w do not have singleton dimensions
+    
+    # Also add units to salt
+    foo['salt'].units = 'g kg-1'
+    foo.close()
+    
+    # and check on the results
+    if Ldir['testing']:
+        foo = nc.Dataset(out_fn)
+        for vn in foo.variables:
+            print('%14s: %s' % (vn, str(foo[vn].shape)))
+        foo.close()
+    
+    # plot for reality check
+    if (Ldir['lo_env'] == 'pm_mac') and Ldir['testing']:
+        a = nc. Dataset(out_fn)
+        import matplotlib.pyplot as plt
+        plt.close('all')
+        fig = plt.figure(figsize=(14,8))
+        
+        v_list = ['salt', 'temp', 'oxygen']
+        NV = len(v_list)
+        ii = 1
+        for vn in v_list:
+            ax = fig.add_subplot(1,NV,ii)
+            ax.plot(a[vn][:].squeeze(), a['z_rho'][:], '-o')
+            if ii == 1:
+                ax.set_ylabel('Z [m]')
+            ax.set_title('%s [%s]' % (vn, a[vn].units))
+            ii += 1
+        a.close()
+        plt.show()
+        
+def get_his_fn_from_dt(Ldir, dt):
+    # This creates the Path of a history file from its datetime
+    date_string = dt.strftime(Ldir['ds_fmt'])
+    his_num = ('0000' + str(dt.hour + 1))[-4:]
+    fn = Ldir['roms_out'] / Ldir['gtagex'] / ('f' + date_string) / ('ocean_his_' + his_num + '.nc')
+    return fn
+
+if Ldir['a1'] == 'blank':
+    folder = 'blank'
+    out_dir = Ldir['LOo'] / 'extract' / 'cast' / folder
+    Lfun.make_dir(out_dir)
+    cruise = 'TEST'
+    sn = 0
+    out_fn = out_dir / (cruise + '_' + str(int(sn)) + '.nc')
+    dt = datetime(2019,7,4,2)
+    fn = get_his_fn_from_dt(Ldir, dt)
+    lon = -123.228000
+    lat = 48.240300
+    get_cast(out_fn, fn, lon, lat)
+    
+                
+# -------------------------------------------------------
+
+# test for success 
+if True:
+    result_dict['result'] = 'success' # success or fail
+else:
+    result_dict['result'] = 'fail'
+
+# *******************************************************
+
+result_dict['end_dt'] = datetime.now()
