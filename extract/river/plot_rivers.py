25b079d0de6e6aae10f357dda3b1dbd933a56ddf
Parker MacCready <p.maccready@gmail.com>
2021-07-28 17:06:13 -0700

    Update plot_rivers.py

--- extract/river/plot_rivers.py
+++ extract/river/plot_rivers.py
@@ -7,53 +7,58 @@ from pathlib import Path
 pth = Path(__file__).absolute().parent.parent.parent / 'alpha'
 if str(pth) not in sys.path:
     sys.path.append(str(pth))
+import Lfun
+import plotting_functions as pfun
 
+import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.dates as mdates
-from datetime import datetime, timedelta
-
-import Lfun
-import zrfun
-import zfun
 
 Ldir = Lfun.Lstart(gridname='cas6', tag='v3')
-fn = Ldir['LOo'] / 'pre' / 'river' / Ldir['gtag'] / 'Data_roms' / 'extraction_2018.01.01_2018.12.31.p'
-df = pd.read_pickle(fn)
+
+# load extraction (an xarray Dataset)
+fn = Ldir['LOo'] / 'pre' / 'river' / Ldir['gtag'] / 'Data_roms' / 'extraction_2018.01.01_2018.12.31.nc'
+x = xr.load_dataset(fn)
 
 # get climatology
-dfi = df.index
-
-cols = df.columns
-
 clm_fn = Ldir['LOo'] / 'pre' / 'river' / Ldir['gtag'] / 'Data_historical' / 'CLIM_flow_1980_2020.p'
-df_clim = pd.read_pickle(clm_fn)
+dfc = pd.read_pickle(clm_fn)
 
-rind = df.index
-dt0 = rind[0]
-dt1 = rind[-1]
+# add the climatology, for practice
+x = x.assign(transport_clim = 0*x.transport)
 
+
+# add the climatology to the xarray dataset
+NT = len(x.time) # needed because the climatology has 366 days
+for rn in list(x.riv.values):
+    if rn in dfc.columns:
+        x.transport_clim.loc[dict(riv=rn)] = dfc[rn][:NT].to_numpy()
+    else:
+        print('Missing ' + rn)
+        
 # plotting
 plt.close('all')
+pfun.start_plot()
+fig = plt.figure()
 
-for ff in range(5): # range(5)
-    
-    fig = plt.figure(figsize=(12,10))
-    
-    for ii in range(9):
-        rr = ii + 9*ff
-        riv_name = df.iloc[:,rr].name
-        ax = fig.add_subplot(3,3,ii+1)
-        df.iloc[:,rr].plot(ax=ax, color='purple')
-        df_clim.iloc[:,rr].plot(ax=ax, color='orange')
-        ax.set_xlim(dt0, dt1)
-        ax.text(.05, .9, riv_name, transform=ax.transAxes, fontweight='bold')
-        for year in range(2018,2021):
-            ax.axvline(x=datetime(year,1,1), c='k')
-        ax.set_xticks([], minor=True)
-        if ii+1 <=6:
-            ax.set_xticklabels([])
-    fig.suptitle(fnr)
-        
+# for time series we are better off using pandas
+df = pd.DataFrame(index=x.time.values)
+ii = 1
+for rn in ['fraser', 'columbia', 'skagit', 'deschutes']:
+    ax = fig.add_subplot(2,2,ii)
+    df.loc[:,'Q'] = x.transport.sel(riv=rn).values
+    df.loc[:,'Qclim'] = x.transport_clim.sel(riv=rn).values
+    if ii == 1:
+        leg = True
+    else:
+        leg = False
+    df.plot(ax=ax, grid=True, legend=leg)
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(x.time.values[0], x.time.values[-1])
+    ax.text(.05,.9,rn.title(), transform=ax.transAxes)
+    if ii in [1,2]:
+        ax.set_xticklabels([])
+    ii += 1
+
 plt.show()
+pfun.end_plot()
