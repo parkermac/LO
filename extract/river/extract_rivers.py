df6e610dd51282ddc6daf7b078bd6fe32224e3e9
Parker MacCready <p.maccready@gmail.com>
2021-08-22 10:39:36 -0700

    Update extract_rivers.py

--- extract/river/extract_rivers.py
+++ extract/river/extract_rivers.py
@@ -92,7 +92,7 @@ for vn in vn_list:
     v_dict[vn] = nanmat.copy()
 tt = 0
 for mds in mds_list:
-    fn = Path('/boildat1').absolute() / 'LiveOcean_output' / Ldir['gtag'] / ('f' + mds) / 'riv2' / 'rivers.nc'
+    fn = Path('/boildat1').absolute() / 'parker' / 'LiveOcean_output' / Ldir['gtag'] / ('f' + mds) / 'riv2' / 'rivers.nc'
     ds = xr.open_dataset(fn)
     # The river transport is given at noon of a number of days surrounding the forcing date.
     # Here we find the index of the time for the day "mds".
