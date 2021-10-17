6db3f44d990f113eb3d82db067fd99a79a45b007
Parker MacCready <p.maccready@gmail.com>
2021-10-17 08:29:35 -0700

    Update test_Ldir.py

--- driver/test_Ldir.py
+++ driver/test_Ldir.py
@@ -3,8 +3,8 @@ Code to test that we are getting the right environment, aimed at
 making sure crontab works.
 
 """
-print('hello')
-# from lo_tools import Lfun
-# Ldir = Lfun.Lstart()
-# for k in Ldir.keys():
-#     print('%s: %s' % (k, Ldir[k]))
+#print('hello')
+from lo_tools import Lfun
+Ldir = Lfun.Lstart()
+for k in Ldir.keys():
+    print('%s: %s' % (k, Ldir[k]))
