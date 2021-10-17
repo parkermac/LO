9bcd3903e8c80ee9db11e99672ad5d9369063012
Parker MacCready <p.maccready@gmail.com>
2021-10-17 09:24:34 -0700

    Update test_Ldir.py

--- driver/test_Ldir.py
+++ driver/test_Ldir.py
@@ -11,8 +11,9 @@ import sys
 pth = Path(__file__).absolute().parent.parent / 'lo_tools' / 'lo_tools'
 if str(pth) not in sys.path:
     sys.path.append(str(pth))
-    
-import Lfun
-Ldir = Lfun.Lstart()
-for k in Ldir.keys():
-    print('%s: %s' % (k, Ldir[k]))
+
+print(str(pth))    
+# import Lfun
+# Ldir = Lfun.Lstart()
+# for k in Ldir.keys():
+#     print('%s: %s' % (k, Ldir[k]))
