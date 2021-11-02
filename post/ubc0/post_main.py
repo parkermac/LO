25ed2d3b90a4d6123a542ba69a5c96437caff182
Parker MacCready <p.maccready@gmail.com>
2021-11-02 08:22:51 -0700

    Update post_main.py

--- post/ubc0/post_main.py
+++ post/ubc0/post_main.py
@@ -1,12 +1,23 @@
 """
 This is the main program for making the extraction for for Susan Allen
-and Doug Latournell at UBC.
+and Doug Latournell at UBC.  It is designed to work mainly for forecasts
+and expects to find 73 hours in a folder of history files.  It still works
+with 25 hours, however, because of the "allhours" flag passed to the
+box extraction.
+
+The code is relatively simple because it relies on the generic box extraction tool.
+
+Note that you need to have a ubc0 job defined in LO_user/extract/box/job_definitions.py.
+
+We also make a tidal average of the box extraction, so the result is much smaller,
+and then we add back the ocean_time dimension with coordinate index of length 1.
 
 Testing on mac:
-run post_main.py -gtx cas6_v3_lo8b -ro 2 -d 2019.07.04 -job ubc0
+run post_main.py -gtx cas6_v3_lo8b -ro 2 -d 2019.07.04 -job ubc0 -test True
+(testing causes it to leave the box extraction around for inspection)
 
-Test on apogee
-run post_main.py -gtx cas6_v0_u0kb -ro 0 -d 2021.10.28 -job ubc0
+Run on apogee
+run post_main.py -gtx cas6_v0_u0kb -ro 0 -d [today's datestring] -job ubc0
 
 """
 
@@ -43,30 +54,29 @@ out_fn0 = out_dir0 / (Ldir['job'] + '_' + Ldir['date_string'] + '_' + Ldir['date
 # this it the name of the file we will copy the output to
 out_dir = Ldir['LOo'] / 'post' / Ldir['gtagex'] / ('f' + Ldir['date_string']) / Ldir['job']
 out_fn_raw = out_dir / 'UBC.nc'
-out_fn = out_dir / 'low_pass_UBC.nc'
+out_fn = out_dir / 'low_passed_UBC.nc'
 out_fn.unlink(missing_ok=True)
 
-if not Ldir['testing']:
-    out_fn_raw.unlink(missing_ok=True)
-    # run extract_box.py to do the actual job
-    tt0 = time()
-    cmd_list = ['python', str(Ldir['LO'] / 'extract' / 'box' / 'extract_box.py'),
-        '-gtx', Ldir['gtagex'], '-ro', str(Ldir['roms_out_num']),
-        '-0', Ldir['date_string'], '-1', Ldir['date_string'],
-        '-lt', 'allhours', '-job', 'ubc0']
-    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
-    stdout, stderr = proc.communicate()
-    print(stdout.decode())
-    if len(stderr) > 0:
-        print(stderr.decode())
-    print('Time for initial extraction = %0.2f sec' % (time()-tt0))
+out_fn_raw.unlink(missing_ok=True)
+# run extract_box.py to do the actual job
+tt0 = time()
+cmd_list = ['python', str(Ldir['LO'] / 'extract' / 'box' / 'extract_box.py'),
+    '-gtx', Ldir['gtagex'], '-ro', str(Ldir['roms_out_num']),
+    '-0', Ldir['date_string'], '-1', Ldir['date_string'],
+    '-lt', 'allhours', '-job', 'ubc0']
+proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+stdout, stderr = proc.communicate()
+print(stdout.decode())
+if len(stderr) > 0:
+    print(stderr.decode())
+print('Time for initial extraction = %0.2f sec' % (time()-tt0))
 
-    # move the extraction to the expected "post" place
-    if Ldir['testing']:
-        # if testing we keep the extraction around to look at
-        shutil.copyfile(out_fn0, out_fn_raw)
-    else:
-        shutil.move(out_fn0, out_fn_raw)
+# move the extraction to the expected "post" place
+if Ldir['testing']:
+    # if testing we keep the extraction around to look at
+    shutil.copyfile(out_fn0, out_fn_raw)
+else:
+    shutil.move(out_fn0, out_fn_raw)
 
 # and do the low pass
 dsr = xr.open_dataset(out_fn_raw)
@@ -89,42 +99,40 @@ ds = ds.expand_dims('ocean_time')
 a = dsr.ocean_time.mean().values
 ds['ocean_time'] = (('ocean_time'), pd.DatetimeIndex([a]))
 # the coordinate step in the line above wasted about a day of my time until I finally tried
-# making if a pandas DatetimeIndex object!!
+# making it a pandas DatetimeIndex object!!
+
+print('Time to make weighted mean: %0.2f sec'% (time()- tt0))
+sys.stdout.flush()
+ds.to_netcdf(out_fn)
+
+dsr.close()
+ds.close()
+
+# compress the resulting file
+tt0 = time()
+ds = xr.load_dataset(out_fn)
+# need to load in order to do the compression
+enc_dict = {'zlib':True, 'complevel':1, '_FillValue':1e20}
+Enc_dict = {vn:enc_dict for vn in ds.data_vars if 's_rho' in ds[vn].dims}
+ds.to_netcdf(out_fn, encoding=Enc_dict)
+ds.close()
+print('Time to compress = %0.2f sec' % (time()- tt0))
 
 if not Ldir['testing']:
+    # clean up
+    out_fn_raw.unlink(missing_ok=True)
 
-    print('Time to make weighted mean: %0.2f sec'% (time()- tt0))
-    sys.stdout.flush()
-    ds.to_netcdf(out_fn)
+print('\nPath to file:\n%s' % (str(out_fn)))
 
-    dsr.close()
-    ds.close()
+# -------------------------------------------------------
 
-    # compress the resulting file
-    tt0 = time()
-    ds = xr.load_dataset(out_fn)
-    # need to load in order to do the compression
-    enc_dict = {'zlib':True, 'complevel':1, '_FillValue':1e20}
-    Enc_dict = {vn:enc_dict for vn in ds.data_vars if 's_rho' in ds[vn].dims}
-    ds.to_netcdf(out_fn, encoding=Enc_dict)
-    ds.close()
-    print('Time to compress = %0.2f sec' % (time()- tt0))
+# test for success
+if out_fn.is_file():
+    result_dict['result'] = 'success'
+else:
+   result_dict['result'] = 'fail'
 
-    if not Ldir['testing']:
-        # clean up
-        out_fn_raw.unlink(missing_ok=True)
+# *******************************************************
 
-    print('\nPath to file:\n%s' % (str(out_fn)))
-
-    # -------------------------------------------------------
-
-    # test for success
-    if out_fn.is_file():
-        result_dict['result'] = 'success'
-    else:
-       result_dict['result'] = 'fail'
-
-    # *******************************************************
-
-    result_dict['end_dt'] = datetime.now()
-    post_argfun.finale(Ldir, result_dict)
+result_dict['end_dt'] = datetime.now()
+post_argfun.finale(Ldir, result_dict)
