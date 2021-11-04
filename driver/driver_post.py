5d65ce05378f8c72166453774be913fd86f6ef32
Parker MacCready <p.maccready@gmail.com>
2021-11-04 14:57:57 -0700

    Update driver_post.py
    now running all jobs

--- driver/driver_post.py
+++ driver/driver_post.py
@@ -103,8 +103,8 @@ while all_found == False:
 if Ldir['testing'] == True:
     job_list = ['daymovie0']
 else:
-    job_list = ['drifters0']
-    #job_list = ['split0', 'surface0', 'layers0', 'ubc0', 'daymovie0', 'critfc0', 'drifters0']
+    #job_list = ['drifters0']
+    job_list = ['split0', 'surface0', 'layers0', 'ubc0', 'daymovie0', 'critfc0', 'drifters0']
 
 for job in job_list:
     
