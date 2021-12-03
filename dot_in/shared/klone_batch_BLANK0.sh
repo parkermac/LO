0d4ec403d98745265af13a8b8b0b6a573debc101
Parker MacCready <p.maccready@gmail.com>
2021-12-03 14:51:52 -0800

    dot_in new ROMS on klone

--- dot_in/shared/klone_batch_BLANK0.sh
+++ dot_in/shared/klone_batch_BLANK0.sh
@@ -20,5 +20,5 @@ module load intel/oneAPI
 NFDIR=/gscratch/macc/local/netcdf-ifort/
 export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
 
-mpirun $roms_ex_dir$/oceanM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
+mpirun $roms_ex_dir$/romsM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
 
