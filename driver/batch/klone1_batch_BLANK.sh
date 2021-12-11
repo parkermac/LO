8a00dfabb77f2762ee107edaa0846db617a0b4a6
Parker MacCready <p.maccready@gmail.com>
2021-12-11 13:25:49 -0800

    Update klone1_batch_BLANK.sh

--- driver/batch/klone1_batch_BLANK.sh
+++ driver/batch/klone1_batch_BLANK.sh
@@ -20,6 +20,6 @@ module load intel/oneAPI
 NFDIR=/gscratch/macc/local/netcdf-ifort/
 export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
 
-## mpirun -np $np_num$ $roms_ex_dir$/oceanM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
-mpirun $roms_ex_dir$/oceanM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
+mpirun -np $np_num$ $roms_ex_dir$/oceanM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
+## mpirun $roms_ex_dir$/oceanM $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
 
