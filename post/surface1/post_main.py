585bcdb7eb1feb10e85fc96b0ee32b6cd90b0015
Parker MacCready <p.maccready@gmail.com>
2021-12-15 10:09:38 -0800

    post/surface1

---
+++ post/surface1/post_main.py
@@ -0,0 +1,129 @@
+"""
+This is the main program for making a box extraction of the daily forecast and sending
+the results to the APL server.
+
+This is for the surface extraction seen on the IOOS EDS Viewer.
+
+Testing on mac:
+run post_main.py -gtx cas6_v3_lo8b -ro 2 -d 2019.07.04 -r backfill -job surface1 -test True
+
+Run for real on apogee:
+python post_main.py -gtx cas6_v0_u0mb -ro 0 -d [today's date string] -r forecast -job surface1
+
+"""
+
+from pathlib import Path
+import sys
+from datetime import datetime, timedelta
+
+from lo_tools import post_argfun
+
+Ldir = post_argfun.intro() # this handles all the argument passing
+result_dict = dict()
+result_dict['start_dt'] = datetime.now()
+
+# ****************** CASE-SPECIFIC CODE *****************
+
+# +++ These are the main things to edit when changing jobs. +++
+
+# Set the job name for LO/extract/box/extract_box.py, which refers to a choice
+# in LO_user/extract/box/job_definitions.py.
+box_job = 'surface0'
+# Note that box_job can be different from Ldir['job'] from the command line
+# arguments.  Ldir['job'] controls the naming of where we archive the extraction:
+# LO_output/post/[gtagex]/[fstring]/Ldir['job']
+
+# This will be used for the file name that goes to the public server.
+share_name = 'surface'
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# imports
+from subprocess import Popen as Po
+from subprocess import PIPE as Pi
+from time import time
+import shutil
+from lo_tools import Lfun
+
+print(' - Creating extraction file for ' + Ldir['date_string'])
+
+# create time range for box extraction
+ds0 = Ldir['date_string']
+dt0 = datetime.strptime(ds0, Lfun.ds_fmt)
+if Ldir['run_type'] == 'backfill':
+    ds1 = ds0
+elif Ldir['run_type'] == 'forecast':
+    ndays = Ldir['forecast_days']
+    dt1 = dt0 + timedelta(days=ndays-1)
+    ds1 = dt1.strftime(Lfun.ds_fmt)
+
+# this is the name of the file created by extract/box/extract_box.py
+out_dir0 = Ldir['LOo'] / 'extract' / Ldir['gtagex'] / 'box'
+out_fn0 = out_dir0 / (box_job + '_surf_' + ds0 + '_' + ds1 + '.nc')
+
+# this it the name of the file we will copy/move the output to
+out_dir = Ldir['LOo'] / 'post' / Ldir['gtagex'] / ('f' + Ldir['date_string']) / Ldir['job']
+out_fn = out_dir / (share_name + '.nc')
+
+# run extract_box.py to do the actual job
+tt0 = time()
+cmd_list = ['python', str(Ldir['LO'] / 'extract' / 'box' / 'extract_box.py'),
+    '-gtx', Ldir['gtagex'], '-ro', str(Ldir['roms_out_num']),
+    '-0', ds0, '-1', ds1, '-lt', 'hourly', '-job', box_job,
+    '-surf', 'True', '-uv_to_rho', 'True']
+proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+stdout, stderr = proc.communicate()
+print(stdout.decode())
+if len(stderr) > 0:
+    print(stderr.decode())
+print('Elapsed time = %0.2f sec' % (time()-tt0))
+
+# copy/move the extraction to the expected "post" place
+if Ldir['testing']:
+    # when testing we keep the original extraction to make it easier to plot
+    shutil.copyfile(out_fn0, out_fn)
+else:
+    shutil.move(out_fn0, out_fn)
+print('\nPath to file:\n%s' % (str(out_fn)))
+
+# copy the file to the server
+if 'apogee' in Ldir['lo_env']:
+    
+    share_user = 'parker@liveocean.apl.uw.edu'
+    print('Warning: copying extractions to server for sharing only works for parker from apogee.')
+    share_dir = '/data/www/liveocean/output/' + 'f' + Ldir['date_string']
+
+    # (i) make the output directory
+    cmd_list = ['ssh', share_user, 'mkdir -p ' + share_dir]
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    stdout, stderr = proc.communicate()
+    Lfun.messages(stdout, stderr, 'Make output directory on server')
+    
+    # (ii) copy the extraction to there
+    cmd_list = ['scp',str(out_fn), share_user + ':' + share_dir]
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    stdout, stderr = proc.communicate()
+    Lfun.messages(stdout, stderr, 'Copying extraction to ' + share_dir)
+        
+    # (iii) then write a little text file to alert users
+    done_fn = out_dir / (share_name + '_done.txt')
+    done_fn.unlink(missing_ok=True)
+    with open(done_fn, 'w') as ffout:
+        ffout.write(datetime.now().strftime('%Y.%m.%d %H:%M:%S'))
+    cmd_list = ['scp',str(done_fn), share_user + ':' + share_dir]
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    stdout, stderr = proc.communicate()
+    Lfun.messages(stdout, stderr, 'Copying done file to server')
+
+# -------------------------------------------------------
+
+# test for success
+if out_fn.is_file():
+    result_dict['result'] = 'success'
+else:
+   result_dict['result'] = 'fail'
+
+# *******************************************************
+
+result_dict['end_dt'] = datetime.now()
+post_argfun.finale(Ldir, result_dict)
