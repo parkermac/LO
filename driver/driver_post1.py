be027cd47e3c4587a812be0072864d95d5d562c3
Parker MacCready <p.maccready@gmail.com>
2021-12-15 16:06:57 -0800

    Update driver_post1.py

--- driver/driver_post1.py
+++ driver/driver_post1.py
@@ -109,7 +109,7 @@ tt0 = time()
 # loop over all jobs
 if Ldir['testing'] == True:
     #job_list = ['sequim1', 'ubc1', 'surface1']
-    job_list = ['layers1']
+    job_list = ['critfc1']
 else:
     job_list = ['surface1', 'layers1', 'ubc1', 'sequim1', 'daymovie0', 'critfc1', 'drifters0', 'archive0']
 
