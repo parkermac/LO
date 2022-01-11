f6c2551fcf05aaa0c37980d0e9b6e3145320b54d
Parker MacCready <p.maccready@gmail.com>
2022-01-11 08:45:45 -0800

    Create make_sta_df.py
    for wcoa2021

---
+++ obs/wcoa2021/make_sta_df.py
@@ -0,0 +1,28 @@
+"""
+Code to make the sta_df.p file for use by extract_casts.py.
+
+This makes a DataFrame with integer Index and columns:
+['Station', 'Longitude', 'Latitude', 'Datetime', 'Cruise']
+
+"""
+
+from lo_tools import Lfun
+Ldir = Lfun.Lstart()
+
+import pandas as pd
+from datetime import datetime, timedelta
+
+in_dir = Ldir['data'] / 'obs' / 'wcoa2021'
+out_dir = Ldir['LOo'] / 'obs' / 'wcoa2021'
+Lfun.make_dir(out_dir)
+
+fn = 'WCOA2021_StationLocations_wTimeandDate.xlsx'
+
+in_fn = in_dir / fn
+a = pd.read_excel(in_fn)
+
+for ii in a.index:
+    a.loc[ii,'Datetime'] = (a.loc[ii,'Date (UTC)'].to_pydatetime()
+        + timedelta(days=a.loc[ii,'Time (UTC)'].hour/24))
+#
+# A.to_pickle(out_dir / 'sta_df.p')
