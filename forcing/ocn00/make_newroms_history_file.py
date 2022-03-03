bf1be6900558991577f0d60024f4cd9e95f58978
Parker MacCready <p.maccready@gmail.com>
2022-03-03 08:15:30 -0800

    housekeeping

---
+++ forcing/ocn00/make_newroms_history_file.py
@@ -0,0 +1,47 @@
+"""
+This is a one-off piece of code to create a 0025 history file for the
+updated ROMS, including bio variables, from an old ROMS file.
+"""
+
+import xarray as xr
+from lo_tools import Lfun, zrfun
+
+Ldir = Lfun.Lstart()
+
+in_fn = Ldir['roms_out2'] / 'cas6_v3_lo8b' / 'f2019.07.04' / 'ocean_his_0025.nc'
+
+out_dir = Ldir['roms_out'] / 'cas6_v0_uu0kb' / 'f2019.07.04'
+Lfun.make_dir(out_dir, clean=True)
+out_fn = out_dir / 'ocean_his_0025.nc'
+
+ds0 = xr.open_dataset(in_fn, decode_times=False)
+
+ds1 = xr.Dataset()
+
+ot_vec = ds0.ocean_time.values
+ds1['ocean_time'] = (('ocean_time',), [ot_vec[0]])
+ds1['ocean_time'].attrs['units'] = Lfun.roms_time_units
+
+roms_names = ['zeta', 'ubar', 'vbar', 'temp', 'salt', 'u', 'v']
+
+bvn_list = ['NO3', 'NH4', 'chlorophyll', 'phytoplankton', 'zooplankton',
+        'LdetritusN', 'SdetritusN', 'LdetritusC', 'SdetritusC',
+        'TIC', 'alkalinity', 'oxygen']
+
+for vn in roms_names + bvn_list:
+    vinfo = zrfun.get_varinfo(vn)
+    if vn in ds0.data_vars:
+        ds1[vn] = (('ocean_time',) + ds0[vn].dims[1:], ds0[vn].values)
+    else:
+        ds1[vn] = (('ocean_time',) + ds0['salt'].dims[1:], 0 * ds0['salt'].values)
+    ds1[vn].attrs['units'] = vinfo['units']
+    ds1[vn].attrs['long_name'] = vinfo['long_name']
+    
+ds0.close()
+        
+# and save to NetCDF
+Enc_dict = {vn:zrfun.enc_dict for vn in ds1.data_vars}
+ds1.to_netcdf(out_fn, encoding=Enc_dict)
+ds1.close()
+
+
---
+++ forcing/riv00/make_forcing_main.py
@@ -0,0 +1,214 @@
+"""
+This is the main program for making the RIVER forcing file, for the
+updated ROMS
+
+Test on mac in ipython:
+
+run make_forcing_main.py -g cas6 -t v0 -r backfill -s continuation -d 2019.07.04 -f riv00 -test True
+
+"""
+
+from pathlib import Path
+import sys, os
+from datetime import datetime, timedelta
+
+from lo_tools import forcing_argfun as ffun
+
+Ldir = ffun.intro() # this handles all the argument passing
+result_dict = dict()
+result_dict['start_dt'] = datetime.now()
+
+# ****************** CASE-SPECIFIC CODE *****************
+
+date_string = Ldir['date_string']
+out_dir = Ldir['LOo'] / 'forcing' / Ldir['gtag'] / ('f' + date_string) / Ldir['frc']
+
+import xarray as xr
+from lo_tools import Lfun, zrfun
+import numpy as np
+import pandas as pd
+import rivfun
+
+if Ldir['testing']:
+    from importlib import reload
+    reload(zrfun)
+    reload(rivfun)
+
+out_fn = out_dir / 'rivers.nc'
+out_fn.unlink(missing_ok=True)
+
+# set up the time index for the record
+dsf = Ldir['ds_fmt']
+dt0 = datetime.strptime(Ldir['date_string'],dsf) - timedelta(days=2.5)
+dt1 = datetime.strptime(Ldir['date_string'],dsf) + timedelta(days=4.5)
+days = (dt0, dt1)
+    
+# pandas Index objects
+dt_ind = pd.date_range(start=dt0, end=dt1)
+yd_ind = pd.Index(dt_ind.dayofyear)
+
+ot_vec = np.array([Lfun.datetime_to_modtime(item) for item in dt_ind])
+NT = len(ot_vec)
+
+S_info_dict = Lfun.csv_to_dict(Ldir['grid'] / 'S_COORDINATE_INFO.csv')
+S = zrfun.get_S(S_info_dict)
+N = S['N']
+
+grid_fn = Ldir['grid'] / 'grid.nc'
+G = zrfun.get_basic_info(grid_fn, only_G=True)
+
+# Load a dataframe with info for rivers to get
+gtag = 'cas6_v3'
+ri_dir = Ldir['LOo'] / 'pre' / 'river' / gtag
+ri_fn = ri_dir / 'river_info.csv'
+ri_df = pd.read_csv(ri_fn, index_col='rname')
+
+# get historical and climatological data files
+year0 = 1980
+year1 = 2020
+# historical and climatological data
+Ldir['Hflow_fn'] = ri_dir / 'Data_historical' / ('ALL_flow_' + str(year0) + '_' + str(year1) + '.p')
+Ldir['Cflow_fn'] = ri_dir / 'Data_historical' / ('CLIM_flow_' + str(year0) + '_' + str(year1) + '.p')
+Ldir['Ctemp_fn'] = ri_dir / 'Data_historical' / ('CLIM_temp_' + str(year0) + '_' + str(year1) + '.p')
+
+# get the list of rivers and indices for this grid
+gri_fn = Ldir['grid'] / 'river_info.csv'
+gri_df = pd.read_csv(gri_fn, index_col='rname')
+if Ldir['testing']:
+    gri_df = gri_df.loc[['columbia', 'skagit'],:]
+NRIV = len(gri_df)
+
+# associate rivers with ones that have temperature climatology data
+ri_df = rivfun.get_tc_rn(ri_df)
+
+# get the flow and temperature data for these days
+qt_df_dict = rivfun.get_qt(gri_df, ri_df, dt_ind, yd_ind, Ldir, dt1, days)
+
+# Start Dataset
+ds = xr.Dataset()
+
+# Add time coordinate
+ds['river_time'] = (('river_time',), ot_vec)
+ds['river_time'].attrs['units'] = Lfun.roms_time_units
+ds['river_time'].attrs['long_name'] = 'river time'
+
+# Add river coordinate
+ds['river'] = (('river',), np.arange(1,NRIV+1))
+ds['river'].attrs['long_name'] = 'river runoff identification number'
+
+# Add river names
+ds['river_name'] = (('river',), list(gri_df.index))
+ds['river_name'].attrs['long_name'] = 'river name'
+
+# Add Vshape
+vn = 'river_Vshape'
+vinfo = zrfun.get_varinfo(vn, vartype='climatology')
+dims = ('s_rho', 'river')
+# For Vtransform = 2, even spacing is a good approximation, and
+# we implement this by using 1/N as the fraction in each vertical cell.
+Vshape = (1/N) * np.ones((N, NRIV))
+ds[vn] = (dims, Vshape)
+ds[vn].attrs['long_name'] = vinfo['long_name']
+
+# Add position and direction
+for vn in ['river_Xposition', 'river_Eposition', 'river_direction']:
+    vinfo = zrfun.get_varinfo(vn, vartype='climatology')
+    if vn == 'river_direction':
+        ds[vn] = (('river',), gri_df.idir.to_numpy())
+    elif vn == 'river_Xposition':
+        X_vec = np.nan * np.ones(NRIV)
+        ii = 0
+        for rn in gri_df.index:
+            if gri_df.loc[rn, 'idir'] == 0:
+                X_vec[ii] = gri_df.loc[rn, 'col_py'] + 1
+            elif gri_df.loc[rn, 'idir'] == 1:
+                X_vec[ii] = gri_df.loc[rn, 'col_py']
+            ii += 1
+        ds[vn] = (('river',), X_vec)
+    elif vn == 'river_Eposition':
+        E_vec = np.nan * np.ones(NRIV)
+        ii = 0
+        for rn in gri_df.index:
+            if gri_df.loc[rn, 'idir'] == 0:
+                E_vec[ii] = gri_df.loc[rn, 'row_py']
+            elif gri_df.loc[rn, 'idir'] == 1:
+                E_vec[ii] = gri_df.loc[rn, 'row_py'] + 1
+            ii += 1
+        ds[vn] = (('river',), E_vec)
+    ds[vn].attrs['long_name'] = vinfo['long_name']
+        
+
+# Add transport
+vn = 'river_transport'
+vinfo = zrfun.get_varinfo(vn, vartype='climatology')
+dims = (vinfo['time_name'],) + ('river',)
+Q_mat = np.zeros((NT, NRIV))
+rr = 0
+for rn in gri_df.index:
+    for rn in gri_df.index:
+        qt_df = qt_df_dict[rn]
+        flow = qt_df['final'].values
+        Q_mat[:,rr] = flow * gri_df.loc[rn, 'isign']
+    rr += 1
+ds[vn] = (dims, Q_mat)
+ds[vn].attrs['long_name'] = vinfo['long_name']
+ds[vn].attrs['units'] = vinfo['units']
+
+# Add salinity and temperature
+for vn in ['river_salt', 'river_temp']:
+    vinfo = zrfun.get_varinfo(vn, vartype='climatology')
+    dims = (vinfo['time_name'],) + ('s_rho', 'river')
+    if vn == 'river_salt':
+        TS_mat = np.zeros((NT, N, NRIV))
+    elif vn == 'river_temp':
+        TS_mat = np.nan * np.zeros((NT, N, NRIV))
+        rr = 0
+        for rn in gri_df.index:
+            qt_df = qt_df_dict[rn]
+            for nn in range(N):
+                TS_mat[:, nn, rr] = qt_df['temperature'].values
+            rr += 1
+    if np.isnan(TS_mat).any():
+        print('Error from riv00: nans in river_temp!')
+        sys.exit()
+    ds[vn] = (dims, TS_mat)
+    ds[vn].attrs['long_name'] = vinfo['long_name']
+    ds[vn].attrs['units'] = vinfo['units']
+    
+# Add biology
+bvn_list = ['NO3', 'NH4', 'LDeN', 'SDeN', 'LDeC', 'SDeC',
+        'TIC', 'alkalinity', 'Oxyg']
+for bvn in bvn_list:
+    vn = 'river_' + bvn
+    vinfo = zrfun.get_varinfo(vn, vartype='climatology')
+    dims = (vinfo['time_name'],) + ('s_rho', 'river')
+    B_mat = np.nan * np.zeros((NT, N, NRIV))
+    rr = 0
+    for rn in gri_df.index:
+        qt_df = qt_df_dict[rn]
+        for nn in range(N):
+            B_mat[:, nn, rr] = rivfun.get_bio_vec(bvn, rn, yd_ind)
+        rr += 1
+    if np.isnan(B_mat).any():
+        print('Error from riv00: nans in B_mat for ' + vn)
+        sys.exit()
+    ds[vn] = (dims, B_mat)
+    ds[vn].attrs['long_name'] = vinfo['long_name']
+    ds[vn].attrs['units'] = vinfo['units']
+
+# Save to NetCDF
+ds.to_netcdf(out_fn)
+ds.close()
+
+# -------------------------------------------------------
+
+# test for success
+if out_fn.is_file():
+    result_dict['result'] = 'success' # success or fail
+else:
+    result_dict['result'] = 'fail'
+
+# *******************************************************
+
+result_dict['end_dt'] = datetime.now()
+ffun.finale(Ldir, result_dict)
