b7bf082b1c8a6f713000580529397144687afd12
Parker MacCready <p.maccready@gmail.com>
2022-04-08 14:54:06 -0700

    Update make_forcing_main.py

--- forcing/atm0/make_forcing_main.py
+++ forcing/atm0/make_forcing_main.py
@@ -51,7 +51,7 @@ elif 'boiler' in Ldir['lo_env']:
     wrf_dir = Path('/data1/darr/wrf_crons/wrfout')
 elif 'apogee' in Ldir['lo_env']:
     # wrf_dir = Path('/boildat/darr/wrf_crons/wrfout')
-    wrf_dir = Path('/pgdat1/parker/wrf')
+    wrf_dir = Path('/pgdat1/parker/LO_data/wrf')
 elif 'perigee' in Ldir['lo_env']:
     wrf_dir = Path('/boildat1/darr/wrf_crons/wrfout')
 # else:
