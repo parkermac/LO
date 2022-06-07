7b431329970a95bdb7b30c9bbc642ca596f55cf0
Parker MacCready <p.maccready@gmail.com>
2022-06-07 13:46:16 -0700

    Update driver_roms2.py
    fix logic for sleep during forecast

--- driver/driver_roms2.py
+++ driver/driver_roms2.py
@@ -88,17 +88,6 @@ if args.run_type == 'forecast':
     ds1 = dt1.strftime(Lfun.ds_fmt)
     
 elif args.run_type == 'backfill': # you have to provide at least ds0 for backfill
-
-    # Don't run backfill at the same time of day as the expected forecast
-    dtnow = datetime.now()
-    iisleep = 1
-    while (dtnow.hour >= 3) and (dtnow.hour <= 7):
-        print('Ten-minute naps:')
-        print(str(iisleep), end=', ')
-        sys.stdout.flush()
-        sleep(600)
-        dtnow = datetime.now()
-        iisleep += 1
     
     ds0 = args.ds0
     if len(args.ds1) == 0:
@@ -132,6 +121,17 @@ def messages(stdout, stderr, mtitle, verbose):
 dt = dt0
 start_type = args.start_type
 while dt <= dt1:
+    
+    # Don't run backfill at the same time of day as the expected forecast
+    dtnow = datetime.now()
+    iisleep = 1
+    while (dtnow.hour >= 3) and (dtnow.hour <= 6):
+        print('Ten-minute naps:')
+        print(str(iisleep), end=', ')
+        sys.stdout.flush()
+        sleep(600)
+        dtnow = datetime.now()
+        iisleep += 1
     
     # fix start_type bug
     if (dt != dt0) and (start_type == 'new'):
