f73363b0b6f402b6ee06eabc1f4391215ee68255
Parker MacCready <p.maccready@gmail.com>
2022-06-20 16:29:34 -0700

    obs/dfo development

--- obs/dfo/test_loadDFO_new.py
+++ obs/dfo/test_loadDFO_new.py
@@ -4,7 +4,7 @@ Code to test the module loadDFO_new.py
 from datetime import datetime, timedelta
 from pathlib import Path
 
-import loadDFO_new as loadDFO
+import loadDFO
 from importlib import reload
 reload(loadDFO)
 
@@ -18,8 +18,8 @@ datelims=(datetime(2016,3,1),datetime(2016,4,1)),
 latlims=(49,49.4),
 lonlims=(-123.6,-123.2)
 
-# df_bot = loadDFO.loadDFO_bottle(basedir=basedir, dbname='DFO.sqlite',
-#     datelims=datelims,latlims=latlims,lonlims=lonlims)
+df_bot = loadDFO.loadDFO_bottle(basedir=basedir, dbname='DFO.sqlite',
+    datelims=datelims,latlims=latlims,lonlims=lonlims)
 
-df_ctd = loadDFO.loadDFO_CTD(basedir=basedir, dbname='DFO_CTD.sqlite',
+df_ctd = loadDFO.loadDFO_CTD(basedir=basedir, dbname='DFO.sqlite',
     datelims=datelims)
