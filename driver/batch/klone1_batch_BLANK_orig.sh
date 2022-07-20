f85ea7f21b6076fdc254d7067fa6d79b98517bd3
Parker MacCready <p.maccready@gmail.com>
2022-07-20 13:01:50 -0700

    Create klone1_batch_BLANK_orig.sh

---
+++ driver/batch/klone1_batch_BLANK_orig.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Job Name
+#SBATCH --job-name=LiveOcean
+
+## Resources
+## Nodes
+#SBATCH --nodes=$node_num$
+## Tasks per node
+#SBATCH --ntasks-per-node=$cores_per_node$
+
+## Walltime 
+#SBATCH --time=02:00:00
+
+## Memory per node
+#SBATCH --mem=100G
+
+module purge
+module load intel/oneAPI
+NFDIR=/gscratch/macc/local/netcdf-ifort/
+export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
+
+mpirun -np $np_num$ $roms_ex_dir$/$roms_ex_name$ $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
+
