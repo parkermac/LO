59d5648d889a91e2a26fb479e038e6ba11b7b545
Parker MacCready <p.maccready@gmail.com>
2022-09-23 13:47:51 -0700

    Update driver_roms3.py

--- driver/driver_roms3.py
+++ driver/driver_roms3.py
@@ -29,6 +29,7 @@ python3 driver_roms3.py -g cas6 -t v00 -x uu0mb -r backfill -s continuation -0 2
 
 production run:
 python3 driver_roms3.py -g cas6 -t v00 -x uu0mb -r backfill -s continuation -0 2021.01.01 -1 2021.01.02 -np 196 -N 28 < /dev/null > uu0mb_a.log &
+python3 driver_roms3.py -g cas6 -t v00 -x uu0mb -r backfill -s perfect -0 2021.01.03 -1 2021.12.31 -np 196 -N 28 < /dev/null > uu0mb_b.log &
 
 """
 
