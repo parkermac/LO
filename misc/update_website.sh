d4f1d1c5fb786cfa6be3113d5f9fcaef50a2079e
Parker MacCready <p.maccready@gmail.com>
2022-09-28 11:34:31 -0700

    Update update_website.sh

--- misc/update_website.sh
+++ misc/update_website.sh
@@ -3,6 +3,6 @@
 ymd=$(date "+%Y.%m.%d")
 echo "Copying movies and tracks to homer for "$ymd
 
-scp /dat1/parker/LO_output/post/cas6_v0_u0mb/f$ymd/daymovie0/*.mp4 pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/Figs_active_forecast
+scp /dat1/parker/LO_output/post/cas6_v0_u0kb/f$ymd/daymovie0/*.mp4 pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/Figs_active_forecast
 scp /dat1/parker/LO_output/tracks/full_surf_forWeb/tracks_full.json pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/tracks/
 scp /dat1/parker/LO_output/tracks/PS_surf_forWeb/tracks_PS.json pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/tracks/
