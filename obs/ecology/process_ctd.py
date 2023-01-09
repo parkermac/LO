6e6e91f7459ce1b81f80748a3fd081734b79e3e1
Parker MacCready <p.maccready@gmail.com>
2023-01-09 15:18:39 -0800

    obs/ecology dev for ctd

--- obs/ecology/process_ctd.py
+++ obs/ecology/process_ctd.py
@@ -1,6 +1,9 @@
 """
 Code to process the ecology ctd data.
 
+Takes a couple minutes to run for 2008-2019. Perhaps I could speed it up
+by making the first, large, multi-year excel file into a csv.
+
 """
 
 import pandas as pd
@@ -19,19 +22,13 @@ in_dir0 = Ldir['data'] / 'obs' / source
 testing = True
 
 if testing:
-    year_list = [2017]
+    year_list = [2019]
 else:
-    year_list = range(2008,2019)
+    year_list = range(2008,2020)
 
 # output location
 out_dir = Ldir['LOo'] / 'obs' / source / otype
 Lfun.make_dir(out_dir)
-
-# This is a dict of all the columns after the initial reading.
-# We add values to a key for any variable we want to save
-v_dict = {
-
-}
 
 # We need to associate lat and lon with each station. They are not stored in the bottle
 # file, but there are station names, with loccations here:
@@ -55,45 +52,128 @@ for year in year_list:
     
     if year == 2017:
         load_data = True
-        ctd_fn = dir0 + 'raw/ParkerMacCready2017CTDDataFeb2018.xlsx'
+        ctd_fn = in_dir0 / 'ParkerMacCready2017CTDDataFeb2018.xlsx'
         sheet_name = '2017Provisional_CTDResults'
     elif year == 2018:
         load_data = True
         #ctd_fn = dir0 + 'raw/Parker_2018.xlsx'
-        ctd_fn = dir0 + 'raw/ParkerMacCready2018CTDDOMar2020.xlsx'
+        ctd_fn = in_dir0 / 'ParkerMacCready2018CTDDOMar2020.xlsx'
         sheet_name = '2018_CTDDOResults'
     elif year == 2019:
         load_data = True
-        ctd_fn = dir0 + 'raw/ParkerMacCready2019CTDDataFeb2020.xlsx'
+        ctd_fn = in_dir0 / 'ParkerMacCready2019CTDDataFeb2020.xlsx'
         sheet_name = '2019Provisional_CTDResults'
     else:
-        ctd_fn = dir0 + 'raw/ParkerMacCready1999-2016CTDDataMay2018.xlsx'
+        ctd_fn = in_dir0 / 'ParkerMacCready1999-2016CTDDataMay2018.xlsx'
         sheet_name = '1999-2016Finalized_CTDResults'
-
-    # data long names; we retain only these fields
     
     # DEFAULTS
     date_col_name = 'Date'
     station_col_name = 'Station'
     depth_col_name = 'Depth'
-    data_new_names = ['Salinity', 'Temperature', 'DO', 'Z']
+    data_new_names = ['SP', 'IT', 'DO (mg/L)', 'z']
     
-    if year == 2017:
-        data_original_names = ['Salinity', 'Temp', 'DO_raw', 'Z']
-    elif year == 2018: # missing Chl
-        #date_col_name = 'UTCDate'
-        #station_col_name = 'SiteCode'
-        #depth_col_name = 'ActualDepthDecimal'
-        data_original_names = ['Salinity', 'Temp', 'DO_adjusted', 'Z']
-        
-    elif year == 2019:
-        data_original_names = ['Salinity', 'Temp', 'DO_raw', 'Z']
+    if year in [2017, 2019]:
+        data_original_names = ['Salinity', 'Temp', 'DO_raw']
     else:
-        data_original_names = ['Salinity', 'Temp', 'DO_adjusted', 'Z']
+        data_original_names = ['Salinity', 'Temp', 'DO_adjusted']
     
     # read in the data (all stations, all casts)
     if load_data:
-        all_casts = pd.read_excel(ctd_fn, sheet_name=sheet_name,
+        df0 = pd.read_excel(ctd_fn, sheet_name=sheet_name,
             parse_dates = [date_col_name])
         load_data = False
+        
+    # add z
+    df0['z'] = -df0[depth_col_name]
+        
+    # select and rename variables
+    v_dict = dict(zip(data_original_names, data_new_names))
+    v_dict['Date'] = 'time'
+    v_dict['Station'] = 'name'
+    v_dict['z'] = 'z'
+    df1 = pd.DataFrame()
+    for v in df0.columns:
+        if v in v_dict.keys():
+            if len(v_dict[v]) > 0:
+                df1[v_dict[v]] = df0[v]
+                
+    # select one year
+    t = pd.DatetimeIndex(df1.time)
+    df = df1.loc[t.year==year,:].copy()
+    
+    # units
+    df['DO (uM)'] = (1000/32) * df['DO (mg/L)']
+    
+    # add lon and lat
+    df['lon'] = np.nan
+    df['lat'] = np.nan
+    for sn in sta_df.index:
+        df.loc[df.name==sn,'lon'] = sta_df.loc[sn,'lon']
+        df.loc[df.name==sn,'lat'] = sta_df.loc[sn,'lat']
+    # and drop stations without good lon, lat, or z
+    df = df[df.lon.notna()]
+    df = df[df.lat.notna()]
+    df = df[df.z.notna()]
+    
+    # calculated quantities
+    SP = df.SP.to_numpy()
+    IT = df.IT.to_numpy()
+    z = df.z.to_numpy()
+    lon = df.lon.to_numpy()
+    lat = df.lat.to_numpy()
+    p = gsw.p_from_z(z, lat)
+    # - do the conversions
+    SA = gsw.SA_from_SP(SP, p, lon, lat)
+    CT = gsw.CT_from_t(SA, IT, p)
+    # - add the results to the DataFrame
+    df['SA'] = SA
+    df['CT'] = CT
+    
+    # Keep only selected columns.
+    cols = ['time', 'lat', 'lon', 'name', 'z', 'CT', 'SA', 'DO (uM)']
+    this_cols = [item for item in cols if item in df.columns]
+    df = df[this_cols]
+    
+    # Generate the cid (cast ID) field.
+    df['cid'] = np.nan
+    # This assume that a cast can be identified by (i) happening on a unique day,
+    # combined with (ii) having a unique name (station name).
+    cid = 0
+    for time in df.time.unique():
+        for name in df.name.unique():
+            df.loc[(df.name==name) & (df.time==time),'cid'] = cid
+            cid += 1
+            
+    # Rework cid to also be increasing from zero in steps of one.
+    a = df.cid.values
+    au = df.cid.unique() # returns uniques in order
+    u_dict = dict(zip(au, np.arange(len(au))))
+    b = np.nan * np.ones(len(a))
+    for ii in u_dict.keys():
+        b[a==ii] = u_dict[ii]
+    df['cid'] = b
+    
+    # Note, the result has casts packed top-to-bottom
+        
+    print(' - processed %d casts' % ( len(df.cid.unique()) ))
+    
+    # add a cruise column
+    df['cruise'] = None
+    
+    if len(df) > 0:
+        # Save the data
+        df.to_pickle(out_fn)
+
+        # Also pull out a dateframe with station info to use for model cast extractions.
+        ind = df.cid.unique()
+        col_list = ['lon','lat','time','name','cruise']
+        info_df = pd.DataFrame(index=ind, columns=col_list)
+        for cid in df.cid.unique():
+            info_df.loc[cid,col_list] = df.loc[df.cid==cid,col_list].iloc[0,:]
+        info_df.index.name = 'cid'
+        info_df['time'] = pd.to_datetime(info_df['time'])
+        info_df.to_pickle(info_out_fn)           
+    
+    
     
---
+++ obs/plot_ctd.py
@@ -0,0 +1,86 @@
+"""
+General-purpose code to look at the output of the ctd processing for a given
+source and year.
+
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from lo_tools import plotting_functions as pfun
+from lo_tools import Lfun
+Ldir = Lfun.Lstart()
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-source', type=str, default='ecology') # e.g. dfo
+parser.add_argument('-year', type=int, default = 2017) # e.g. 2019
+
+args = parser.parse_args()
+
+if args.source=='nceiSalish':
+    aa = [-125.5, -122, 47, 50]
+elif args.source=='ecology':
+    aa = [-125, -122, 46, 50]
+else:
+    aa = [-130, -122, 42, 52]
+x0, x1, y0, y1 = aa
+
+otype = 'ctd'
+
+df = pd.read_pickle(Ldir['LOo'] / 'obs' / args.source / otype / (str(args.year) + '.p'))
+
+# keep only data in a box
+df = df[(df['lon']>x0) & (df['lon']<x1) & (df['lat']>y0) & (df['lat']<y1)]
+zbot = df.z.min()
+
+plt.close('all')
+pfun.start_plot()
+
+fig = plt.figure(figsize=(22,12))
+
+vn_list = ['SA', 'CT', 'DO (uM)']
+ax_list = [1,2,4]
+ax_dict = dict(zip(vn_list,ax_list))
+
+# map axis
+axm = fig.add_subplot(133)
+
+for vn in vn_list:
+    if vn in df.columns:
+        ii = ax_dict[vn]
+        ax = fig.add_subplot(2,3,ii)
+        for mo in range(1,13):
+            dfm = df[df['time'].dt.month==mo]
+            if len(dfm) > 0:
+                    dfm.plot(x=vn,y='z', ax=ax, style='.',
+                        color=pfun.month_color_dict[mo], legend=False,
+                        markersize=1)
+                    if vn == 'SA':
+                        dfm.plot(x='lon',y='lat', ax=axm, style='o',
+                            color=pfun.month_color_dict[mo], legend=False,
+                            markersize=26-2*mo)
+        # add month labels
+        if ii == 1:
+            for mo in range(1,13):
+                ax.text(.05, .95 - .065*mo, pfun.month_name_dict[mo],
+                    color=pfun.month_color_dict[mo], fontweight='bold',
+                    transform=ax.transAxes)
+        ax.set_xlabel('')
+        ax.set_ylim(bottom=zbot-20)
+        # ax.set_xlim(left=0)
+        ax.text(.95,.05,vn,transform=ax.transAxes,ha='right',fontweight='bold')
+        if ii in [1,5,9]:
+            ax.set_ylabel('Z [m]')
+    
+pfun.add_coast(axm)
+pfun.dar(axm)
+axm.axis(aa)
+axm.set_title('%s %s Data for %d' % (args.source, otype.upper(), args.year))
+axm.set_xlabel('Longitude')
+axm.set_ylabel('Latitude')
+
+plt.show()
+pfun.end_plot()
