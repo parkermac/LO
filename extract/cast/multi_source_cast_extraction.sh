2c2c69d0ccaf197c55cf4002eed26d3e5c3ce494
Parker MacCready <p.maccready@gmail.com>
2023-01-13 13:05:19 -0800

    Create multi_source_cast_extraction.sh

---
+++ extract/cast/multi_source_cast_extraction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run multiple cast extraction jobs at once
+
+# Example run command:
+# ./multi_source_cast_extraction cas6_v0_live
+
+otype=bottle
+year=2017
+
+for source in ecology dfo nceiCoastal nceiSalish
+do
+ python ./extract_casts.py -gtx $1 -source $source -otype $otype -year $year > $source.log &
+done
