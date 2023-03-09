17602e28f8bb9bacb38ab478c691262cfbd37dc2
Parker MacCready <p.maccready@gmail.com>
2023-03-09 12:35:36 -0800

    Update multi_source_cast_extraction.sh

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -7,7 +7,8 @@
 
 otype=bottle
 year=2017
-for source in ecology dfo1 nceiCoastal nceiSalish
+# for source in ecology dfo1 nceiCoastal nceiSalish
+for source in dfo1
 do
  python ./extract_casts_fast.py -gtx $1 -source $source -otype $otype -year $year > $source_$otype_$year.log &
 done
