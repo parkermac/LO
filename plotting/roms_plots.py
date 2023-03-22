b8d89e9f2e0edc253ae6d4102c2cd901276849d9
Parker MacCready <p.maccready@gmail.com>
2023-03-22 12:06:33 -0700

    Update roms_plots.py
    added P_basic_PS

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -80,6 +80,52 @@ def P_basic(in_dict):
     else:
         plt.show()
 
+def P_basic_PS(in_dict):
+    # Like P_basic but focused on Puget Sound
+    # START
+    ds = xr.open_dataset(in_dict['fn'])
+    # find aspect ratio of the map
+    aa = [-123.2, -122.1, 47, 48.4]
+    # AR is the aspect ratio of the map: Vertical/Horizontal
+    AR = (aa[3] - aa[2]) / (np.sin(np.pi*aa[2]/180)*(aa[1] - aa[0]))
+    fs = 14
+    hgt = 10
+    pfun.start_plot(fs=fs, figsize=(int(hgt*2.5/AR),int(hgt)))
+    fig = plt.figure()
+    # PLOT CODE
+    vn_list = ['salt', 'temp']
+    ii = 1
+    for vn in vn_list:
+        if in_dict['auto_vlims']:
+            pinfo.vlims_dict[vn] = ()
+        ax = fig.add_subplot(1, len(vn_list), ii)
+        cs = pfun.add_map_field(ax, ds, vn, pinfo.vlims_dict,
+                cmap=pinfo.cmap_dict[vn], fac=pinfo.fac_dict[vn], vlims_fac=pinfo.range_dict[vn])
+        fig.colorbar(cs)
+        pfun.add_coast(ax)
+        ax.axis(aa)
+        pfun.dar(ax)
+        ax.set_title('Surface %s %s' % (pinfo.tstr_dict[vn],pinfo.units_dict[vn]), fontsize=1.2*fs)
+        ax.set_xlabel('Longitude')
+        if ii == 1:
+            ax.set_ylabel('Latitude')
+            pfun.add_info(ax, in_dict['fn'])
+            #pfun.add_windstress_flower(ax, ds)
+            pfun.add_bathy_contours(ax, ds, txt=True)
+        elif ii == 2:
+            ax.set_yticklabels([])
+            pfun.add_velocity_vectors(ax, ds, in_dict['fn'])
+        ii += 1
+    #fig.tight_layout()
+    # FINISH
+    ds.close()
+    pfun.end_plot()
+    if len(str(in_dict['fn_out'])) > 0:
+        plt.savefig(in_dict['fn_out'])
+        plt.close()
+    else:
+        plt.show()
+
 def P_Fb(in_dict):
     # START
     ds = xr.open_dataset(in_dict['fn'])
