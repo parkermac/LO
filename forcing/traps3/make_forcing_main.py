8a6893df43efd142c8f4c596a7f1f03b3734101c
Parker MacCready <p.maccready@gmail.com>
2023-05-12 14:02:50 -0700

    Update make_forcing_main.py

--- forcing/traps3/make_forcing_main.py
+++ forcing/traps3/make_forcing_main.py
@@ -3,7 +3,7 @@ This is the main program for making the RIVER and TRAPS forcing file, for the
 updated ROMS
 
 Test on pc in ipython:
-run make_forcing_main.py -g cas6 -r backfill -d 2021.01.01 -f traps2 -test True
+run make_forcing_main.py -g cas6 -r backfill -d 2017.01.01 -f traps3 -test True
 
 2023.04.11 Updated to work with pre/river1 data format.
 
