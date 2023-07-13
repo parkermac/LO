0091c3e395c5091648dfd0a94c4b032a9acba59a
Parker MacCready <p.maccready@gmail.com>
2023-07-13 12:52:04 -0700

    Update klone3_batch_BLANK.sh

--- driver/batch/klone3_batch_BLANK.sh
+++ driver/batch/klone3_batch_BLANK.sh
@@ -18,6 +18,9 @@
 ## bug fix 2022.09.14
 ##. /opt/ohpc/admin/lmod/lmod/init/profile
 
+echo -e "Pre: $(date)\n" # Timestamp to .out file before anything else
+env                      # Dump the environment to .out
+
 module purge
 module load intel/oneAPI
 NFDIR=/gscratch/macc/local/netcdf-ifort/
@@ -25,3 +28,8 @@ export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
 
 mpirun -np $np_num$ $roms_ex_dir$/$roms_ex_name$ $roms_out_dir$/liveocean.in > $roms_out_dir$/log.txt
 
+MPIRUN_EXIT_CODE=$?  # Capture exit code of mpirun
+
+echo -e "\nPost: $(date) with mpirun exit code: $MPIRUN_EXIT_CODE" # Timestamp to .out just before exit
+
+exit $MPIRUN_EXIT_CODE  # Exit script with exit code from mpirun
