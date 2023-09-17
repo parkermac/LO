41bd14ced73f2c4df17aff487c3a4c22bd2f3612
Parker MacCready <p.maccready@gmail.com>
2023-09-17 08:32:46 -0700

    layers2 for wgh

---
+++ post/layers2/make_layers.py
@@ -0,0 +1,193 @@
+"""
+This creates a layers file for a single time.
+
+To test on mac:
+
+run make_layers -test True
+"""
+
+from pathlib import Path
+import argparse
+import xarray as xr
+from time import time
+from PyCO2SYS import CO2SYS
+import seawater as sw
+from warnings import filterwarnings
+filterwarnings('ignore') # skip some warning messages from CO2YS
+import numpy as np
+import sys
+
+from lo_tools import plotting_functions as pfun
+
+# defaults for testing
+from lo_tools import Lfun
+Ldir = Lfun.Lstart()
+in_fn = (Ldir['roms_out'] /
+    'wgh2_t0_xn0b' / 'f2023.09.14' / 'ocean_his_0002.nc')
+out_fn = (Ldir['parent'] / 'LO_output' / 'post' /
+    'wgh2_t0_xn0b' / 'f2023.09.14' / 'layers2' / 'tempfiles' / 'layers_000000.nc')
+Lfun.make_dir(out_fn.parent)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-in_fn', default=in_fn, type=str)
+parser.add_argument('-out_fn', default=out_fn, type=str)
+parser.add_argument('-test', '--testing', default=False, type=Lfun.boolean_string)
+
+args = parser.parse_args()
+in_fn = Path(args.in_fn)
+out_fn = Path(args.out_fn)
+testing = args.testing
+
+out_fn.unlink(missing_ok=True)
+
+in_ds = xr.open_dataset(in_fn, decode_times=False)
+out_ds = xr.Dataset()
+
+# add time
+out_ds['ocean_time'] = (('ocean_time'), in_ds.ocean_time.values)
+out_ds['ocean_time'].attrs['units'] = Ldir['roms_time_units']
+# add coordinates and other static 2-D fields
+vn_list = [ 'lon_rho', 'lat_rho', 'mask_rho', 'h']
+for vn in vn_list:
+    out_ds[vn] = (('eta_rho', 'xi_rho'), in_ds[vn].values)
+    out_ds[vn].attrs['long_name'] = in_ds[vn].attrs['long_name']
+    try:
+        out_ds[vn].attrs['units'] = in_ds[vn].attrs['units']
+    except KeyError:
+        pass
+
+# Get ready to initialize the data layers.
+if testing:
+    depth_list = ['10']
+else:
+    depth_list = ['surface', '10', '20', '30', '50', 'bottom']
+
+tag_dict = dict()
+for depth in depth_list:
+    if depth in ['surface', 'bottom']:
+        tag_dict[depth] = ' at ' + depth
+    else:
+        tag_dict[depth] = ' at ' + depth + ' m depth'
+
+if testing:
+    # vn_in_list = ['temp', 'salt']
+    # vn_out_list = ['temp', 'salt']
+    vn_in_list = ['temp', 'salt', 'alkalinity', 'TIC']
+    vn_out_list = ['temp', 'salt' , 'PH', 'ARAG']
+else:
+    # vn_in_list = ['temp', 'salt', 'phytoplankton', 'NO3', 'oxygen', 'rho', 'alkalinity', 'TIC']
+    vn_in_list = ['temp', 'salt', 'phytoplankton', 'NO3', 'oxygen', 'alkalinity', 'TIC']
+    vn_out_list = ['temp', 'salt', 'phytoplankton', 'NO3', 'oxygen', 'PH', 'ARAG']
+
+do_carbon = True
+# this flag is introduced to handle different testing choices
+if 'TIC' not in vn_in_list:
+    do_carbon = False
+
+# create zfull to use with the pfun.get_laym() function
+zfull = pfun.get_zfull(in_ds, in_fn, 'rho')
+in_mask_rho = in_ds.mask_rho.values # 1 = water, 0 = land
+
+def get_layer(vn, depth, in_ds, zfull, in_mask_rho):
+    if depth == 'surface':
+        L = in_ds[vn][0,-1,:,:].values
+    elif depth == 'bottom':
+        L = in_ds[vn][0,0,:,:].values
+    else:
+        L = pfun.get_laym(in_ds, zfull, in_mask_rho, vn, -float(depth))
+    return L
+
+def get_Ld(depth, in_ds, in_mask_rho):
+    # makes a field of layer depth (m)
+    if depth == 'surface':
+        Ld = 0 * np.ones(in_mask_rho.shape)
+    elif depth == 'bottom':
+        Ld = in_ds.h.values
+    else:
+        Ld = float(depth) * np.ones(in_mask_rho.shape)
+    Ld[in_mask_rho == 0] = np.nan
+    return Ld
+
+# Fill the layers
+for depth in depth_list:
+    if testing:
+        print(' - Depth = ' + depth)
+    suffix = '_' + depth
+    v_dict = dict()
+    tt0 = time()
+    for vn in vn_in_list:
+        v_dict[vn] = get_layer(vn, depth, in_ds, zfull, in_mask_rho)
+    if testing:
+        print('   -- fill v_dict took %0.2f sec' % (time()-tt0))
+        
+    if do_carbon:
+        # ------------- the CO2SYS steps -------------------------
+        tt0 = time()
+        # ===============================================================
+        # OLD
+        # Ld = get_Ld(depth, in_ds, in_mask_rho)
+        # lat = in_ds.lat_rho.values
+        # # create pressure
+        # Lpres = sw.pres(Ld, lat)
+        # # get in situ temperature from potential temperature
+        # Ltemp = sw.ptmp(v_dict['salt'], v_dict['temp'], 0, Lpres)
+        # # convert from umol/L to umol/kg using in situ dentity
+        # Lalkalinity = 1000 * v_dict['alkalinity'] / (v_dict['rho'] + 1000)
+        # Lalkalinity[Lalkalinity < 100] = np.nan
+        # LTIC = 1000 * v_dict['TIC'] / (v_dict['rho'] + 1000)
+        # LTIC[LTIC < 100] = np.nan
+        # CO2dict = CO2SYS(Lalkalinity, LTIC, 1, 2, v_dict['salt'], Ltemp, Ltemp,
+        #     Lpres, Lpres, 50, 2, 1, 10, 1, NH3=0.0, H2S=0.0)
+        # ===============================================================
+        # NEW: using gsw to create in-situ density
+        import gsw
+        lon = in_ds.lon_rho.values
+        lat = in_ds.lat_rho.values
+        Ld = get_Ld(depth, in_ds, in_mask_rho)
+        Lpres = gsw.p_from_z(-Ld, lat) # pressure [dbar]
+        SA = gsw.SA_from_SP(v_dict['salt'], Lpres, lon, lat)
+        CT = gsw.CT_from_pt(SA, v_dict['temp'])
+        rho = gsw.rho(SA, CT, Lpres) # in situ density
+        Ltemp = gsw.t_from_CT(SA, CT, Lpres) # in situ temperature
+        # convert from umol/L to umol/kg using in situ dentity
+        Lalkalinity = 1000 * v_dict['alkalinity'] / rho
+        Lalkalinity[Lalkalinity < 100] = np.nan
+        LTIC = 1000 * v_dict['TIC'] / rho
+        LTIC[LTIC < 100] = np.nan
+        CO2dict = CO2SYS(Lalkalinity, LTIC, 1, 2, v_dict['salt'], Ltemp, Ltemp,
+            Lpres, Lpres, 50, 2, 1, 10, 1, NH3=0.0, H2S=0.0)
+        # ===============================================================
+        # NOTE that the "in" and "out" versions of the returned variables will be
+        # identical because we pass the same pressure and temperature for
+        # input and output (in-situ in both cases)
+        PH = CO2dict['pHout']
+        v_dict['PH'] = PH.reshape((v_dict['salt'].shape))
+        ARAG = CO2dict['OmegaARout']
+        v_dict['ARAG'] = ARAG.reshape((v_dict['salt'].shape))
+        if testing:
+            print('   -- carbon took %0.2f sec' % (time()-tt0))
+        # --------------------------------------------------------
+    
+    # Write data to the output file.
+    for vn in vn_out_list:
+        out_ds[vn + suffix] = (('ocean_time', 'eta_rho', 'xi_rho'), v_dict[vn].reshape((1,) + in_mask_rho.shape))
+        if vn == 'PH':
+            out_ds[vn + suffix].attrs['long_name'] = 'pH' + tag_dict[depth]
+        elif vn == 'ARAG':
+            out_ds[vn + suffix].attrs['long_name'] = 'Aragonite Saturation State' + tag_dict[depth]
+        else:
+            out_ds[vn + suffix].attrs['long_name'] = in_ds[vn].attrs['long_name'] + tag_dict[depth]
+        try:
+            out_ds[vn + suffix].attrs['units'] = in_ds[vn].attrs['units']
+        except KeyError:
+            pass
+    sys.stdout.flush()
+
+
+in_ds.close()
+
+enc_dict = {'zlib':True, 'complevel':1, '_FillValue':1e20}
+Enc_dict = {vn:enc_dict for vn in out_ds.data_vars if 'ocean_time' in out_ds[vn].dims}
+out_ds.to_netcdf(out_fn, unlimited_dims='ocean_time', encoding=Enc_dict)
+# NOTE: we use unlimited_dims so that ncrcat knows what to do later
+out_ds.close()
---
+++ post/layers2/post_main.py
@@ -0,0 +1,130 @@
+"""
+This is the main program for making a box extraction of the daily forecast and sending
+the results to the APL server.
+
+It creates a single NetCDF file containing fields on selected levels
+from the history files in a given day.
+
+Testing on mac:
+run post_main.py -gtx wgh2_t0_xn0b -ro 0 -d 2023.09.14 -r backfill -job layers2 -test True
+
+Run for real on apogee:
+python post_main.py -gtx wgh2_t0_xn0b -ro 0 -d [today's date string] -r forecast -job layers2
+
+"""
+
+from pathlib import Path
+import sys
+from datetime import datetime, timedelta
+
+from lo_tools import post_argfun
+
+Ldir = post_argfun.intro() # this handles all the argument passing
+result_dict = dict()
+result_dict['start_dt'] = datetime.now()
+
+# ****************** CASE-SPECIFIC CODE *****************
+
+# This will be used for the file name that goes to the public server.
+share_name = 'layers'
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# imports
+from subprocess import Popen as Po
+from subprocess import PIPE as Pi
+from time import time
+import shutil
+from lo_tools import Lfun
+import numpy as np
+
+print(' - Creating extraction file for ' + Ldir['date_string'])
+
+# this is where the output ends up
+out_dir = Ldir['LOo'] / 'post' / Ldir['gtagex'] / ('f' + Ldir['date_string']) / Ldir['job']
+out_fn = out_dir / (share_name + '.nc')
+temp_dir = out_dir / 'tempfiles'
+Lfun.make_dir(temp_dir, clean=True)
+
+# create time range for extraction
+ds0 = Ldir['date_string']
+dt0 = datetime.strptime(ds0, Lfun.ds_fmt)
+if Ldir['run_type'] == 'backfill':
+    ds1 = ds0
+elif Ldir['run_type'] == 'forecast':
+    ndays = Ldir['forecast_days']
+    dt1 = dt0 + timedelta(days=ndays-1)
+    ds1 = dt1.strftime(Lfun.ds_fmt)
+fn_list = Lfun.get_fn_list('hourly', Ldir, ds0, ds1)
+if Ldir['testing']:
+    fn_list = fn_list[:2]
+else:
+    fn_list = fn_list[::4]
+
+# do the extractions
+N = len(fn_list)
+proc_list = []
+tt0 = time()
+print('Working on ' + Ldir['job'] + ' (' + str(N) + ' times)')
+for ii in range(N):
+    in_fn = fn_list[ii]
+    sys.stdout.flush()
+    count_str = ('000000' + str(ii))[-6:]
+    temp_out_fn = temp_dir / ('layers_' + count_str + '.nc')
+    this_dir = str(Ldir['LO'] / 'post' / Ldir['job']) + '/'
+    cmd_list = ['python', this_dir + 'make_layers.py', '-in_fn', str(in_fn),
+        '-out_fn', str(temp_out_fn), '-test', str(Ldir['testing'])]
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    proc_list.append(proc)
+        
+    # Nproc controls how many ncks subprocesses we allow to stack up
+    # before we require them all to finish.
+    # NOTE: we add the (ii > 0) because otherwise it starts by doing a single
+    # job, and in this case the jobs are long enough for that to be a significant
+    # slowdown.
+    if ((np.mod(ii,Ldir['Nproc']) == 0) and (ii > 0)) or (ii == N-1):
+        for proc in proc_list:
+            stdout, stderr = proc.communicate()
+            # make sure everyone is finished before continuing
+            if True:
+                if len(stdout) > 0:
+                    print('\n'+stdout.decode())
+                if len(stderr) > 0:
+                    print('\n'+stderr.decode())
+        proc_list = []
+    ii += 1
+    
+# concatenate the records into one file
+# This bit of code is a nice example of how to replicate a bash pipe
+pp1 = Po(['ls', str(temp_dir)], stdout=Pi)
+pp2 = Po(['grep','layers'], stdin=pp1.stdout, stdout=Pi)
+cmd_list = ['ncrcat','-p', str(temp_dir), '-O', str(out_fn)]
+proc = Po(cmd_list, stdin=pp2.stdout, stdout=Pi, stderr=Pi)
+stdout, stderr = proc.communicate()
+if True:
+    if len(stdout) > 0:
+        print('\n'+stdout.decode())
+    if len(stderr) > 0:
+        print('\n'+stderr.decode())
+print('Time for full layers extraction = %0.2f sec' % (time()- tt0))
+
+if not Ldir['testing']:
+    Lfun.make_dir(temp_dir, clean=True)
+
+print('\nPath to file:\n%s' % (str(out_fn)))
+
+# copy the file to the server
+post_argfun.copy_to_server(Ldir, out_fn)
+
+# -------------------------------------------------------
+
+# test for success
+if out_fn.is_file():
+    result_dict['result'] = 'success'
+else:
+   result_dict['result'] = 'fail'
+
+# *******************************************************
+
+result_dict['end_dt'] = datetime.now()
+post_argfun.finale(Ldir, result_dict)
