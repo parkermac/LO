8b8893175db4a0f614c218a0d54b30bf8d4052d9
Parker MacCready <p.maccready@gmail.com>
2023-09-25 16:22:56 -0700

    Create copy_river_files.py

---
+++ misc/copy_river_files.py
@@ -0,0 +1,24 @@
+"""
+This is a program to copy river forcing files from boiler onto apogee.
+"""
+
+from pathlib import Path
+import pandas as pd
+from lo_tools import Lfun
+import shutil
+
+in_dir0 = Path('/boildat/parker/LiveOcean_output/cas6_v3')
+
+out_dir0 = Path('/dat1/parker/LO_output/forcing/cas6_v0')
+
+date_range = pd.date_range(start='2016-12-15', end='2016-12-16')#'2021-10-15')
+
+f_list = ['f'+ item.strftime(Lfun.ds_fmt) for item in date_range]
+
+for f in f_list:
+    in_file = in_dir0 / f / 'riv2' / 'rivers.nc'
+    out_dir = out_dir0 / f / 'riv0'
+    try:
+        shutil.copy(in_file,out_dir)
+    except Exception as e:
+        print(e)
