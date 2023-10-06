ae73be54cd7dde37ce265cc53b8e0b3771e0da15
Parker MacCready <p.maccready@gmail.com>
2023-10-06 08:18:42 -0700

    Update multi_source_cast_extraction.sh

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -3,19 +3,20 @@
 # Run multiple cast extraction jobs at once
 
 # Example run command:
-# ./multi_source_cast_extraction.sh cas6_v0_live
+# ./multi_source_cast_extraction.sh cas6_v0_live 0 [my run on perigee]
+# ./multi_source_cast_extraction.sh cas7_trapsV00_meV00 3 [Aurora's run on perigee]
 
 otype=bottle
 year=2017
 for source in ecology dfo1 nceiCoastal nceiSalish
 # for source in dfo1
 do
- python ./extract_casts_fast.py -gtx $1 -source $source -otype $otype -year $year > $source_$otype_$year.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > $source_$otype_$year.log &
 done
 
 otype=ctd
 year=2017
 for source in dfo1 ecology
 do
- python ./extract_casts_fast.py -gtx $1 -source $source -otype $otype -year $year > $source_$otype_$year.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > $source_$otype_$year.log &
 done
