a2011d60bbd35e4ae2b55cd885f2dacb82c3ca60
Parker MacCready <p.maccready@gmail.com>
2023-11-19 10:27:22 -0800

    Update forcing3_all_jobs_by_year.sh

--- driver/forcing3_all_jobs_by_year.sh
+++ driver/forcing3_all_jobs_by_year.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-year=2017
-gridname=cas2k
-python ./driver_forcing3.py -g $gridname -0 $year.01.02 -1 $year.12.31 -f ocn00 > ocn00_$year.log &
-python ./driver_forcing3.py -g $gridname -0 $year.01.02 -1 $year.12.31 -f atm00 > atm00_$year.log &
-python ./driver_forcing3.py -g $gridname -0 $year.01.02 -1 $year.12.31 -f riv00 > riv00_$year.log &
-python ./driver_forcing3.py -g $gridname -0 $year.01.02 -1 $year.12.31 -f tide00 > tide00_$year.log &
+year=2012
+ymd0=$year.10.07
+ymd1=$year.12.31
+gridname=cas7
+python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f ocn01 > ocn01_$year.log &
+python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f atm00 > atm00_$year.log &
+python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f traps00 > traps00_$year.log &
+python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f tide00 > tide00_$year.log &
