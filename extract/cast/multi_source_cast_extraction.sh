a4293fd27f8b5516dabf8197e118ae53f0f6f00a
Parker MacCready <p.maccready@gmail.com>
2023-12-05 09:22:05 -0800

    Update multi_source_cast_extraction.sh

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Run multiple cast extraction jobs at once
+# Run multiple cast extraction jobs at once, does both bottles and casts, using
+# all available sources
 
 # Example run command:
-# ./multi_source_cast_extraction.sh cas6_v0_live 0 [my run on perigee]
-# ./multi_source_cast_extraction.sh cas7_trapsV00_meV00 3 [Aurora's run on perigee]
+# ./multi_source_cast_extraction.sh cas6_v0_live 0 2017 [my run on perigee]
+# ./multi_source_cast_extraction.sh cas7_trapsV00_meV00 3 2017 [Aurora's run on perigee]
+
+# The three required command line arguments are:
+# 1. gtagex
+# 2. roms out number
+# 3. year
 
 otype=bottle
-year=2017
 for source in ecology dfo1 nceiCoastal nceiSalish
-# for source in dfo1
 do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > $source_$otype_$year.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > $1_$source_$otype_$3.log &
 done
 
 otype=ctd
-year=2017
 for source in dfo1 ecology
 do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > $source_$otype_$year.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > $1_$source_$otype_$3.log &
 done
