8d852f0acb13a8c0290b24d55e4c9f97f9c06fd2
Parker MacCready <p.maccready@gmail.com>
2024-01-01 11:06:30 -0800

    Update forcing3_all_jobs_by_year.sh
    slight change to quoting. Should not change anything.

--- driver/forcing3_all_jobs_by_year.sh
+++ driver/forcing3_all_jobs_by_year.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 year=$1
-ymd0=$year.01.01
-ymd1=$year.12.31
+ymd0=$year".01.01"
+ymd1=$year".12.31"
 gridname=cas7
 python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f ocn01 > ocn01_$year.log &
 python ./driver_forcing3.py -g $gridname -0 $ymd0 -1 $ymd1 -f atm00 > atm00_$year.log &
