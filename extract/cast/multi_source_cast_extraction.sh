a6835d8d1a054e9062e149514ac2dbf67d02918b
Parker MacCready <p.maccready@gmail.com>
2024-01-01 11:04:10 -0800

    Update multi_source_cast_extraction.sh
    fix log file naming

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Run multiple cast extraction jobs at once, does both bottles and casts, using
-# all available sources
+# all available sources. Note the use of "_" in naming the log file, which is
+# required, otherwise bash thinks I am referencing the variables $source_ and $otype_
+# which do not exist.
 
-# Example run command:
+# Example run commands:
 # ./multi_source_cast_extraction.sh cas6_v0_live 0 2017 [my run on perigee]
 # ./multi_source_cast_extraction.sh cas7_trapsV00_meV00 3 2017 [Aurora's run on perigee]
+# ./multi_source_cast_extraction.sh cas7_t0_x4b 0 2013 [Long Hindcast on apogee]
 
 # The three required command line arguments are:
 # 1. gtagex
@@ -15,11 +18,11 @@
 otype=bottle
 for source in ecology dfo1 nceiCoastal nceiSalish
 do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > $1_$source_$otype_$3.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > ./$1"_"$source"_"$otype"_"$3.log &
 done
 
 otype=ctd
 for source in dfo1 ecology
 do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > $1_$source_$otype_$3.log &
+ python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > ./$1"_"$source"_"$otype"_"$3.log &
 done
