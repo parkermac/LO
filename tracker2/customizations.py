4add7e1f235e90c3fdbac60d7cd690cd40ec28e3
Parker MacCready <p.maccready@gmail.com>
2024-08-01 13:45:47 -0700

    Update customizations.py

--- tracker2/customizations.py
+++ tracker2/customizations.py
@@ -2,15 +2,16 @@
 This is a module of experiment-specific customizations that are applied
 within tracker.
 
-It was motivated by the Nina Bednarsek experiments where she wanted larvae
-(rockfish?) to stay at one depth for the first month and then go to a different
-depth for the second month.
-
 It is not clear to me the best way to implement such specific behaviors in the
 code, so this module is an attempt.
 """
 
 def update_TR(nd, TR):
+    """
+    This was motivated by the Nina Bednarsek experiments where she wanted larvae
+    (rockfish?) to stay at one depth for the first month and then go to a different
+    depth for the second month.
+    """
     # Note that because TR is a dictionary, changing it in the function
     # changes it everywhere. No need to return it.
     
