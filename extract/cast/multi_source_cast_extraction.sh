6c1fd0d7febc34d22c4014c304d8735f03f6eace
Parker MacCready <p.maccready@gmail.com>
2024-08-17 09:47:16 -0700

    Update multi_source_cast_extraction.sh

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Run multiple cast extraction jobs at once, does both bottles and casts, using
+# Run multiple cast extraction jobs for many years. Does both bottles and casts, using
 # all available sources. Note the use of "_" in naming the log file, which is
 # required, otherwise bash thinks I am referencing the variables $source_ and $otype_
 # which do not exist.
 
 # Example run commands:
-# ./multi_source_cast_extraction.sh cas6_v0_live 0 2017 [my run on perigee]
-# ./multi_source_cast_extraction.sh cas7_trapsV00_meV00 3 2017 [Aurora's run on perigee]
-# ./multi_source_cast_extraction.sh cas7_t0_x4b 0 2013 [Long Hindcast on apogee]
+# ./multi_source_cast_extraction.sh cas7_t0_x4b 0 > cas7.log &
 
-# The three required command line arguments are:
+# The two required command line arguments are:
 # 1. gtagex
 # 2. roms out number
-# 3. year
 
-otype=bottle
-for source in ecology dfo1 nceiCoastal nceiSalish
+for year in 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023
 do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > ./$1"_"$source"_"$otype"_"$3.log &
-done
 
-otype=ctd
-for source in dfo1 ecology
-do
- python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $3 > ./$1"_"$source"_"$otype"_"$3.log &
+    otype=bottle
+    for source in dfo1 ecology_nc nceiSalish nceiCoastal LineP nceiPNW WOD
+    do
+    python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > ./$1"_"$source"_"$otype"_"$year.log
+    done
+
+    otype=ctd
+    for source in dfo1 ecology_nc LineP NHL ocnms_ctd
+    do
+    python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > ./$1"_"$source"_"$otype"_"$year.log
+    done
+
 done
