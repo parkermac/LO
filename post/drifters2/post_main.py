267515af7f2cea8390831bbf2a9365901aa322d4
Parker MacCready <p.maccready@gmail.com>
2024-09-08 09:50:04 -0700

    Update post_main.py
    drifters 2 dev

--- post/drifters2/post_main.py
+++ post/drifters2/post_main.py
@@ -30,22 +30,29 @@ from subprocess import PIPE as Pi
 import numpy as np
 import json
 import xarray as xr
+import pandas as pd
+from lo_tools import Lfun
+import pytz
 
 print('*** Creating drifter files for ' + Ldir['date_string'] + ' ***')
 dsr = Ldir['date_string']
 if Ldir['testing']:
+    dsr0 = dsr
     dtt = 1
 else:
-    dtt = 3 # assume this is for a daily forecast, so we track for three days
+    dsr_dt00 = datetime.strptime(dsr,Ldir['ds_fmt'])
+    dsr_dt0 = dsr_dt00 - timedelta(days=6)
+    dsr0 = datetime.strftime(dsr_dt0,Ldir['ds_fmt'])
+    dtt = 9 # assume this is for a daily forecast, so we track three days into the future.
 
-# RUN TRACKER JOBS - parallelize using subprocess
+# RUN TRACKER JOBS
 tt0 = time()
-exp_list = ['dmMerhab2', 'full', 'PS']
+exp_list = ['PS']
 three_flag = 'False'
 for exp in exp_list:
     cmd = ['python', str(Ldir['LO']) + '/tracker2/tracker.py',
         '-gtx', Ldir['gtagex'], '-ro', str(Ldir['roms_out_num']),
-        '-d', dsr, '-dtt', str(dtt),
+        '-d', dsr0, '-dtt', str(dtt),
         '-exp', exp, '-3d', three_flag,
         '-sub_tag', 'forWeb', '-clb', 'True']
     proc = Po(cmd,stdout=Pi, stderr=Pi)
@@ -66,14 +73,24 @@ for exp in exp_list:
         three_tag = '3d'
     else:
         three_tag = 'surf'
-    out_fn_dict[exp] = Ldir['LOo'] / 'tracks2' / Ldir['gtagex'] / (exp + '_' + three_tag + '_forWeb') / ('release_' + dsr + '.nc')
-    out_json_dict0[exp] = Ldir['LOo'] / 'tracks2' / Ldir['gtagex'] / (exp + '_' + three_tag + '_forWeb') / ('tracks.json')
-    out_json_dict1[exp] = Ldir['LOo'] / 'tracks2' / Ldir['gtagex'] / (exp + '_' + three_tag + '_forWeb') / ('times.json')
+    out_fn_dict[exp] = Ldir['LOo'] / 'tracks2' / Ldir['gtagex'] / (exp + '_' + three_tag + '_forWeb') / ('release_' + dsr0 + '.nc')
+    # output location for jsons will be in one folder that is easy to copy, to help with debugging
+    json_dir = Ldir['LOo'] / 'tracks2' / Ldir['gtagex'] / 'json_files'
+    out_json_dict0[exp] = json_dir / (exp+'_tracks.json')
+    out_json_dict1[exp] = json_dir / (exp+'_times.json')
 result = 'SUCCESS'
 for exp in exp_list:
     if not out_fn_dict[exp].is_file():
         result = 'FAIL'
 # END OF TRACKER JOBS
+
+# function to get local time
+def get_dt_local(dt, tzl='US/Pacific'):
+    tz_utc = pytz.timezone('UTC')
+    tz_local = pytz.timezone(tzl)
+    dt_utc = dt.replace(tzinfo=tz_utc)
+    dt_local = dt_utc.astimezone(tz_local)
+    return dt_local
 
 # CONVERT TO JSON AND SCP TO HOMER
 for exp in exp_list:
@@ -81,6 +98,8 @@ for exp in exp_list:
     in_fn = out_fn_dict[exp]
     out_fn0 = out_json_dict0[exp]
     out_fn1 = out_json_dict1[exp]
+    Lfun.make_dir(json_dir)
+    print('Writing jsons to: ' + str(json_dir))
 
     ds = xr.open_dataset(in_fn)
     # packed time, particle
@@ -88,22 +107,21 @@ for exp in exp_list:
     lat = ds['lat'].values
     NT, NP = lon.shape
 
-    # Make a time vector (Note the Time is hours from the start of the release)
-    dt0 = datetime.strptime(dsr, Ldir['ds_fmt'])
-    Time = ds.Time.values
-    dt_list = []
-    for h in Time:
-        dt_list.append(dt0 + timedelta(days=h/24))
-
+    # Make a time vector as a list of strings for the time slider, converted to local time.
+    dt0 = ds.ot.values
+    dti0 = pd.DatetimeIndex(dt0)
+    tt_list = []
+    for dtu in dti0:
+        dt_local = get_dt_local(dtu)
+        tt_list.append(datetime.strftime(dt_local,'%m/%d/%Y - %I%p')+' '+dt_local.tzname())
+    
     # Create and save jsons.
+    # 1. Position
     xy = []
     for pp in range(NP):
         xy.append({'x': [('%0.3f' % (item)) for item in lon[:,pp]], 'y': [('%0.3f' % (item)) for item in lat[:,pp]]})
     json.dump(xy, open(out_fn0, 'w'))
-
-    tt_list = []
-    for tt in range(NT):
-        tt_list.append(datetime.strftime(dt_list[tt],'%Y-%m-%d %H:%M'))
+    # 2. Time
     tt = [{'t': tt_list}]
     json.dump(tt, open(out_fn1, 'w'))
     
