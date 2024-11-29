79315be8def68d03f86e6f99ffe8b5782d927738
Parker MacCready <p.maccready@gmail.com>
2024-11-29 15:23:49 -0800

    Update get_lo_info.py

--- get_lo_info.py
+++ get_lo_info.py
@@ -72,8 +72,10 @@ elif (str(HOME) == '/home/parker') & ('perigee' in HOSTNAME):
 
 elif (str(HOME) == '/home/parker') & ('apogee' in HOSTNAME):
     lo_env = 'pm_apogee'
-    roms_out1 = Path('/pgdat1/parker/LO_roms')
-    roms_out2 = Path('/pgdat2/parker/LO_roms')
+    roms_out1 = Path('/dat1/parker/LO_roms')
+    roms_out2 = Path('/dat2/parker/LO_roms')
+    roms_out3 = Path('/pgdat1/parker/LO_roms')
+    roms_out4 = Path('/pgdat2/parker/LO_roms')
 
 elif (str(HOME) == '/usr/lusers/pmacc'):
     lo_env = 'pm_mox'
