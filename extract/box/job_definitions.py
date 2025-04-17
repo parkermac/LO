1b5036a78cf88b26c266a2b00766167bc4b969bd
Parker MacCready <p.maccready@gmail.com>
2025-04-17 14:21:39 -0700

    misc

--- extract/box/job_definitions.py
+++ extract/box/job_definitions.py
@@ -122,5 +122,8 @@ def get_box(job, Lon, Lat):
     elif job == 'townsend':
         aa = [-125, -124, 42.3, 46.3]
         vn_list = 'h,mask_rho,pm,pn,u,v,salt,temp'
+    elif job == 'smolt0':
+        aa = [-129, -123.5, 46, 51.5]
+        vn_list = 'h,mask_rho,pm,pn,u,v,salt,temp,phytoplankton'
         
     return aa, vn_list
