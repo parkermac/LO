2f3d0a56a72899b12205d86fd299539bbcf0dbfe
Parker MacCready <p.maccready@gmail.com>
2025-05-04 08:33:17 -0700

    Update speed_test.py

--- misc/speed_test.py
+++ misc/speed_test.py
@@ -1,12 +1,15 @@
 """
 Code to test access speed for kopah.
 
-Results:
+Results, all for 24 files:
 
-mac: 6 sec for 24 files
-apogee: 15 sec for 24 files
-klone: 18 sec for 24 files (cpu-g2, standard access)
-klone: 96 sec for 24 files (cpu-g2, s3 access)
+mac: 6 sec
+apogee: 15 sec
+klone: 18 sec (cpu-g2, standard access)
+klone: 96 sec (cpu-g2, s3 access)
+klone: 44 sec (cpu-g2, https access)
+
+NOTE: I had to add the files to kopah using --acl-public to get it to work.
 
 """
 
@@ -20,7 +23,7 @@ from time import time
 Ldir = Lfun.Lstart()
 
 # test of standard access
-#fstr = 'f2017.07.04'
+#fstr = 'f2017.07.04' # test on mac
 fstr = 'f2025.05.03'
 in_dir = Ldir['parent'] / 'LO_roms' / 'cas7_t0_x4b' / fstr
 
@@ -31,6 +34,7 @@ for ii in range(2,26):
     hh = ('0000' + str(ii))[-4:]
     fn = in_dir / ('ocean_his_' + hh + '.nc')
     if False:
+        # standard access
         ds = xr.open_dataset(fn)
         a += ds.salt.to_numpy()
         ds.close()
