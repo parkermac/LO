68308ddcf816a2ed6ad5bbf76fd4b94f9b3ec12b
Parker MacCready <p.maccready@gmail.com>
2025-06-04 12:53:39 -0700

    Update multi_source_cast_extraction.sh

--- extract/cast/multi_source_cast_extraction.sh
+++ extract/cast/multi_source_cast_extraction.sh
@@ -12,19 +12,20 @@
 # 1. gtagex
 # 2. roms out number
 
-for year in 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023
+# for year in 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023
+for year in 2017
 do
 
     otype=bottle
-    for source in nceiCoastal LineP nceiPNW WOD
-    # for source in dfo1 ecology_nc nceiSalish nceiCoastal LineP nceiPNW WOD
+    # for source in nceiCoastal LineP nceiPNW WOD
+    for source in dfo1 ecology_nc nceiSalish nceiCoastal LineP nceiPNW WOD
     do
     python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > ./$1"_"$source"_"$otype"_"$year.log
     done
 
     otype=ctd
-    for source in LineP NHL
-    # for source in dfo1 ecology_nc LineP NHL ocnms_ctd
+    # for source in LineP NHL
+    for source in dfo1 ecology_nc LineP NHL ocnms_ctd
     do
     python ./extract_casts_fast.py -gtx $1 -ro $2 -source $source -otype $otype -year $year > ./$1"_"$source"_"$otype"_"$year.log
     done
