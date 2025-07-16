be970e941e9edf0962af2b93e7c4d9cf6896aec1
Parker MacCready <p.maccready@gmail.com>
2025-07-16 08:22:54 -0700

    Create klone_job_test.sh

---
+++ driver/klone_job_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Test of using klone compute nodes.
+
+source ~/.bashrc
+srun -p compute -A macc --pty bash -l
+conda activate loenv
+echo "Date = $(date)" > job_test.txt
+conda deactivate
+logout
