6db91a6eb68b2db93b8bcc37e6c12fa211d093b3
ajleeson <aurorajleeson@gmail.com>
2025-08-19 18:37:07 -0700

    make figures by default

--- pre/trapsP01/climatology_all_source_types.sh
+++ pre/trapsP01/climatology_all_source_types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ./make_climatology_moh20_tinyrivs.py
-python ./make_climatology_moh20_LOrivbio.py
-python ./make_climatology_moh20_wwtp.py
-python ./make_climatology_was24_wwtp.py
+python ./make_climatology_moh20_tinyrivs.py -test True
+python ./make_climatology_moh20_LOrivbio.py -test True
+python ./make_climatology_moh20_wwtp.py -test True
+python ./make_climatology_was24_wwtp.py -test True
