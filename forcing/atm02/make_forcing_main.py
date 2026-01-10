5bb92baf6a40af70af625225ab1a9be95b3176bb
Parker MacCready <p.maccready@gmail.com>
2026-01-10 08:50:44 -0800

    Update make_forcing_main.py

--- forcing/atm02/make_forcing_main.py
+++ forcing/atm02/make_forcing_main.py
@@ -306,13 +306,17 @@ if planB == False:
             rrmat[-1,:,:] = rrmat[-2,:,:]
             D[vn] = rrmat
 
-        # # HACK 2025.12.23 for negative Qair that showed up in a small patch
-        # # and runined the forecast.
-        # if vn == 'Qair':
-        #     qmat = D[vn].copy()
-        #     qmat[qmat<30] = 30
-        #     D[vn] = qmat
-        # # END HACK
+        # Enforce limits on Tair and Qair 2026.01.10
+        if vn == 'Qair':
+            qmat = D[vn].copy()
+            qmat[qmat<0] = 0
+            qmat[qmat>100] = 100
+            D[vn] = qmat
+        if vn == 'Tair':
+            tmat = D[vn].copy()
+            tmat[tmat<-20] = -20
+            tmat[tmat>45] = 45
+            D[vn] = tmat
         
         ds[vn] = (dims, D[vn])
         ds[vn].attrs['units'] = vinfo['units']
