06166d991a5cb7a3dddad2d93ab86531b4492871
Parker MacCready <p.maccready@gmail.com>
2026-02-15 12:32:18 -0800

    Create get_wrf.py

---
+++ driver/get_wrf.py
@@ -0,0 +1,42 @@
+"""
+Code to download WRF forcing files from Atm. Sci. server.
+"""
+
+import sys, os
+from datetime import datetime, timedelta
+from subprocess import Popen as Po
+from subprocess import PIPE as Pi
+from time import time, sleep
+
+# Add the path to lo_tools by hand so that it we can import Lfun on klone
+# without loenv. In general we write code to run on klone using only the
+# default python3 installation.
+pth = Path(__file__).absolute().parent.parent / 'lo_tools' / 'lo_tools'
+if str(pth) not in sys.path:
+    sys.path.append(str(pth))
+import Lfun
+
+Ldir = Lfun.Lstart()
+
+out_dir0 = Ldir['data'] / 'wrf'
+dstr = datetime.now().strftime('%Y%m%d') + '00'
+out_dir = out_dir0 / dstr
+Lfun.make_dir(out_dir)
+
+print(dstr)
+
+acct_dict = Lfun.csv_to_dict(Ldir['data'] / 'accounts' / 'wrf_pm_2026.02.15.csv')
+
+cmd_list = ['wget','--no-check-certificate','-e','robots=off','-r','--level=1','--accept','0000','-nc','-c',
+    '--http-user='+acct_dict['username'],
+    '--http-passwd='+acct_dict['password'],
+    '-P',str(out_dir),
+    'https://a.atmos.uw.edu/mm5rt/puget_sound/'+dstr+'/']
+
+tt0 = time()
+proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+stdout, stderr = proc.communicate()
+if len(stderr) > 0:
+    print('Error getting WRF files for %s' % (dstr))
+print('Time to get WRF files = %0.1f sec' % (time()-tt0))
+
