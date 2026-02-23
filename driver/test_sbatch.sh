502094eefcd80cc34bdfa507ffb4b29d3730c9f3
Parker MacCready <p.maccready@gmail.com>
2026-02-23 08:34:38 -0800

    Create test_sbatch.sh

---
+++ driver/test_sbatch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Nodes
+#SBATCH --nodes=1
+
+## Tasks per node
+#CBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+
+## Walltime 
+#SBATCH --time=00:01:00
+
+## Use all memory on the node [0 to use all, or 128G]
+#SBATCH --mem=1G
+
+# Do not return until the job is finished
+#SBATCH --wait
+
+echo -e "Pre: $(date)\n" > /gscratch/macc/parker/sbatch_test.txt
