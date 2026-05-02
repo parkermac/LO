7def0053f7c6809c4e6fe51b44346dddf86f5f00
Parker MacCready <p.maccready@gmail.com>
2026-05-02 09:06:34 -0700

    Create speed_test.sh

---
+++ extract/to_zarr/speed_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## run using
+# sbatch -p cpu-g2 -A macc ./speed_test.sh &
+
+## Nodes
+#SBATCH --nodes=1
+
+## Tasks per node
+#SBATCH --ntasks-per-node=1
+
+## Walltime 
+#SBATCH --time=01:00:00
+
+## Set memory use. Each slice (32 cores) has 256G
+#SBATCH --mem=128G
+
+# Do not return until the job is finished
+#SBATCH --wait
+
+## Assume xarray to_zarr() uses multi-threading.
+#SBATCH --cpus-per-task=10
+
+source /gscratch/macc/parker/miniconda3/etc/profile.d/conda.sh
+conda activate loenv
+# These exports, along with the cpus-per-task set above
+# allow fast multi-threading in python.
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK      # For OpenMP
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK # For OpenBLAS
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK      # For Intel MKL
+export VECLIB_MAXIMUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+dir0='/gscratch/macc/parker'
+dir1=${dir0}'/LO/extract/to_zarr'
+
+python3 ${dir1}/speed_test.py > ${dir1}/speed_test.log
+
