9b7b6163d252df37443ce8519d6f3d2c6e2813aa
Parker MacCready <p.maccready@gmail.com>
2026-05-02 08:59:01 -0700

    Create speed_test.py

---
+++ extract/to_zarr/speed_test.py
@@ -0,0 +1,34 @@
+"""
+Code to test speed of accessing netcdf vs. zarr arrays.
+
+run as
+
+pmsrun2
+conda activate loenv
+python3 speed_test.py
+
+"""
+
+from time import time
+import xarray as xr
+from lo_tools import Lfun
+
+Ldir = Lfun.Lstart()
+
+fn0 = Ldir['roms_out'] / 'cas7_t2_x11b' / 'f2026.05.01' / 'ocean_his_0001.nc'
+
+fn1 = 'https://s3.kopah.uw.edu/liveocean-pmacc/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/h_01.zarr'
+
+tt0 = time()
+ds = xr.open_dataset(fn0)
+a = ds.salt.values
+b = a*a
+ds.close()
+print('netcdf time = %0.3f' % (time()-tt0))
+
+tt0 = time()
+ds = xr.open_zarr(fn1)
+a = ds.salt.values
+b = a*a
+ds.close()
+print('zarr time = %0.3f' % (time()-tt0))
