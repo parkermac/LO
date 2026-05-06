29a8230b9b9c05e97f760039faecced01379b7be
Parker MacCready <p.maccready@gmail.com>
2026-05-06 09:38:45 -0700

    Update worker_to_zarr.py

--- extract/to_zarr/worker_to_zarr.py
+++ extract/to_zarr/worker_to_zarr.py
@@ -23,10 +23,11 @@ time_str = datetime.now().strftime(time_format)
 tt0 = time()
 
 f_string = 'f' + args.ds0
+bucket_name = 'liveocean-pmacc'
 
 hh = ('000' + args.his_str)[-4:]
 cmd_list = ['s5cmd','cp',
-    's3://'+bucket_name+'/LO_roms/'+args.gtx+'/'+f_string+'/ocean_his_' + hh + '.nc']
+    's3://'+bucket_name+'/LO_roms/'+args.gtx+'/'+f_string+'/ocean_his_' + hh + '.nc',
     '/var/tmp/']
 proc = Po(cmd_list, stdout=Pi, stderr=Pi)
 stdout, stderr = proc.communicate()
