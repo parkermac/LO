6373e3d50ceb2dd21d97d1258d7f82c718ed9917
Parker MacCready <p.maccready@gmail.com>
2026-05-06 08:56:49 -0700

    to_zarr dev

--- extract/to_zarr/driver_to_zarr.sh
+++ extract/to_zarr/driver_to_zarr.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 ## run using
-# sbatch -p cpu-g2 -A macc ./driver_to_zarr.sh &
+# sbatch --array1-25 ./driver_to_zarr.sh  cas7_t2_x11b 2026.05.01 &
 
-## Nodes
+## Group
+#SBATCH -A macc
+
+## Node type
+#SBATCH -p cpu-g2
+
+## Number of nodes
 #SBATCH --nodes=1
 
-## Tasks per node
-#SBATCH --ntasks-per-node=1
+## Tasks per node (number of history files in a day)
+#SBATCH --ntasks-per-node=25
 
 ## Walltime 
 #SBATCH --time=01:00:00
 
-## Set memory use. Each slice (32 cores) has 256G
-#SBATCH --mem=128G
+## Set memory use. Each slice (32 cores) has 256G. Use 0 to use all.
+#SBATCH --mem=0
 
 # Do not return until the job is finished
 #SBATCH --wait
 
-## Assume xarray to_zarr() uses multi-threading.
-#SBATCH --cpus-per-task=10
+## Assume some python code uses multi-threading.
+#SBATCH --cpus-per-task=6
+## Note: 25*6 = 150, which is less than the 192 coes we have on a node.
+
+## Reserve all the cores on this node.
+#SBATCH --exclusive
 
 source /gscratch/macc/parker/miniconda3/etc/profile.d/conda.sh
 conda activate loenv
@@ -31,17 +41,17 @@ export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK      # For Intel MKL
 export VECLIB_MAXIMUM_THREADS=$SLURM_CPUS_PER_TASK
 export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
+# Path to this directory
 dir0='/gscratch/macc/parker'
 dir1=${dir0}'/LO/extract/to_zarr'
 
-gtx='cas7_t2_x11b'
-ds0='2026.05.01'
-his_str='01'
+gtx=$1
+ds0=$2
 
-# put the file in /var/tmp
+# put the files for this day in /var/tmp
 bucket='liveocean-pmacc'
-s5cmd cp "s3://"${bucket}"/LO_roms/"${gtx}"/f"${ds0}"/ocean_his_00"${his_str}".nc" "/var/tmp/h_"${his_str}".nc"
+s5cmd sync "s3://"${bucket}"/LO_roms/"${gtx}"/f"${ds0}"/*.nc" "/var/tmp/"
 
-python3 ${dir1}/worker_to_zarr.py -his_str ${his_str} > ${dir1}/test.log
+python3 ${dir1}/worker_to_zarr.py -his_str $SLURM_ARRAY_TASK_ID > ${dir1}"/test_"$SLURM_ARRAY_TASK_ID".log"
 
-s5cmd cp --acl public-read "/var/tmp/h_"${his_str}".zarr" "s3://"${bucket}"/LO_roms/"${gtx}"_zarr/f"${ds0}"/"
+s5cmd sync --acl public-read "/var/tmp/*.zarr" "s3://"${bucket}"/LO_roms/"${gtx}"_zarr/f"${ds0}"/"
--- extract/to_zarr/worker_to_zarr.py
+++ extract/to_zarr/worker_to_zarr.py
@@ -1,27 +1,31 @@
 """
 Code to convert a single netcdf file to zarr.
-
-Save to kopah?
 """
 
 import xarray as xr
 from time import time
+from datetime import datetime, timedelta
 
 import argparse
 import sys
 from lo_tools import Lfun
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-his_str', type=str, default='01')
+parser.add_argument('-his_str', type=str)
 args = parser.parse_args()
 
-tt0 = time()
+time_format = '%Y.%m.%d %H:%M:%S'
+time_str = datetime.now().strftime(time_format)
 
-in_fn = '/var/tmp/h_' + args.his_str + '.nc'
-out_fn = '/var/tmp/h_' + args.his_str + '.zarr'
+hh = ('000' + args.his_str)[-4:]
+
+in_fn = '/var/tmp/ocean_his_' + hh + '.nc'
+out_fn = '/var/tmp/ocean_his_' + hh + '.zarr'
+
+tt0 = time()
 
 ds = xr.open_dataset(in_fn)
 ds.to_zarr(out_fn)
 
-print('time for to_zarr = %0.1f sec' % (time()-tt0))
+print('%s: time for to_zarr = %0.1f sec' % (time_str, time()-tt0))
 sys.stdout.flush()
