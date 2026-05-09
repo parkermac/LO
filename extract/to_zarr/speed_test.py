8bf91d78a3d18e796c1d60a0ea3c21997599c291
Parker MacCready <p.maccready@gmail.com>
2026-05-09 08:37:56 -0700

    Update speed_test.py
    updated using copilot in xcode

--- extract/to_zarr/speed_test.py
+++ extract/to_zarr/speed_test.py
@@ -6,7 +6,6 @@ Code to test speed of accessing netcdf vs. zarr arrays.
 from time import time
 import xarray as xr
 from lo_tools import Lfun
-import fsspec
 
 Ldir = Lfun.Lstart()
 
@@ -17,26 +16,29 @@ if local:
     fn0 = Ldir['roms_out'] / 'cas7_t2_x11b' / 'f2026.05.01' / 'ocean_his_0001.nc'
     fn1 = '/gscratch/macc/parker/tmp/h_01.zarr'
 else:
-    # kopah version
-    fn0 = 'https://s3.kopah.uw.edu/liveocean-pmacc/tmp/ocean_his_0001.nc'
-    fn1 = 'https://s3.kopah.uw.edu/liveocean-pmacc/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
+    # s3 version with direct access
+    fn0 = 's3://liveocean-pmacc/tmp/ocean_his_0001.nc'
+    fn1 = 's3://liveocean-pmacc/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
+    storage_options = {'client_kwargs': {'endpoint_url': 'https://s3.kopah.uw.edu'}, 'anon': True}
 
 tt0 = time()
 
 if local:
     ds = xr.open_dataset(fn0)
 else:
-    fs = fsspec.filesystem('https')
-    ds = xr.open_dataset(fs.open(fn0))
+    ds = xr.open_dataset(fn0, engine='h5netcdf', storage_options=storage_options)
     
-a = ds.salt.values
+a = ds.salt[0,-1,:,:].values
 b = a*a
 ds.close()
 print('netcdf time = %0.3f' % (time()-tt0))
 
 tt0 = time()
-ds = xr.open_zarr(fn1)
-a = ds.salt.values
+if local:
+    ds = xr.open_zarr(fn1)
+else:
+    ds = xr.open_zarr(fn1, storage_options=storage_options)
+a = ds.salt[0,-1,:,:].values
 b = a*a
 ds.close()
 print('zarr time = %0.3f' % (time()-tt0))
