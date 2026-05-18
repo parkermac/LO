37a00f0fae18f396270c79220f43e329cef226e8
Parker MacCready <p.maccready@gmail.com>
2026-05-18 15:10:11 -0700

    zarr dev

--- extract/to_zarr/speed_test.py
+++ extract/to_zarr/speed_test.py
@@ -19,6 +19,7 @@ else:
     # s3 version with direct access
     fn0 = 's3://liveocean-pmacc/tmp/ocean_his_0001.nc'
     fn1 = 's3://liveocean-pmacc/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
+    fn1alt = 'https://s3.kopah.uw.edu/liveocean-pmacc/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
     storage_options = {'client_kwargs': {'endpoint_url': 'https://s3.kopah.uw.edu'}, 'anon': True}
 
 tt0 = time()
@@ -28,7 +29,7 @@ if local:
 else:
     ds = xr.open_dataset(fn0, engine='h5netcdf', storage_options=storage_options)
     
-a = ds.salt[0,-1,:,:].values
+a = ds.salt[0,:,10,10].values
 b = a*a
 ds.close()
 print('netcdf time = %0.3f' % (time()-tt0))
@@ -37,8 +38,9 @@ tt0 = time()
 if local:
     ds = xr.open_zarr(fn1)
 else:
-    ds = xr.open_zarr(fn1, storage_options=storage_options)
-a = ds.salt[0,-1,:,:].values
+    # ds = xr.open_zarr(fn1, storage_options=storage_options)
+    ds = xr.open_zarr(fn1alt)
+a = ds.salt[0,:,10,10].values
 b = a*a
 ds.close()
 print('zarr time = %0.3f' % (time()-tt0))
