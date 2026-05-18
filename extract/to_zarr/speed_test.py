b9935fd564d152adf85e05f00e86c52b2177a5ad
Parker MacCready <p.maccready@gmail.com>
2026-05-18 15:29:57 -0700

    Update speed_test.py

--- extract/to_zarr/speed_test.py
+++ extract/to_zarr/speed_test.py
@@ -18,7 +18,7 @@ if local:
 else:
     # s3 version with direct access
     fn0 = 's3://liveocean-pmacc/tmp/ocean_his_0001.nc'
-    fn1 = 's3://liveocean-test/LO_roms/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
+    fn1 = 's3://liveocean-test/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
     fn1alt = 'https://s3.kopah.uw.edu/liveocean-test/cas7_t2_x11b_zarr/f2026.05.01/ocean_his_0001.zarr'
     storage_options = {'client_kwargs': {'endpoint_url': 'https://s3.kopah.uw.edu'}, 'anon': True}
 
