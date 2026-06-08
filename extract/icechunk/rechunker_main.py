24c6d07db467a45d7d1c47f0f3ea2e8e445f09c3
Parker MacCready <p.maccready@gmail.com>
2026-06-08 09:02:35 -0700

    icechunk dev

--- extract/icechunk/rechunker_main.py
+++ extract/icechunk/rechunker_main.py
@@ -12,11 +12,18 @@ The smaller subset will be all the hours in a day for hourly list_type, or all t
 
 Testing:
 
-run rechunker_main -gtx cas7_t2_x11b -0 2024.01.01 -1 2024.01.02 -lt hourly -test True
+run rechunker_main -gtx cas7_t2_x11b -0 2024.01.01 -1 2024.01.02 -lt hourly0 -test True
 
 run rechunker_main -gtx cas7_t1_x11ab -0 2024.01.01 -1 2024.02.29 -lt average -test True
 
+Note, using list_type = hourly0 zero means that on the first day we will get 25 hours (0:24)
+and on subsequent days it will revert to hourly, getting 24 hours (1:24).
+
+When populating a NEW collection you should use hourly0
+but when adding to it later you should use hourly.
+
 """
+
 # imports
 import sys
 import argparse
@@ -31,9 +38,9 @@ import pandas as pd
 from pathlib import Path
 from datetime import datetime, timedelta
 
-# pid = os.getpid()
-# print(' rechunker_main '.center(60,'='))
-# print('PID for this job = ' + str(pid))
+pid = os.getpid()
+print(' rechunker_main '.center(60,'='))
+print('PID for this job = ' + str(pid))
 
 # command line arugments
 parser = argparse.ArgumentParser()
@@ -42,7 +49,7 @@ parser.add_argument('-gtx', '--gtagex', type=str)   # e.g. cas7_t2_x11b
 # select time period and frequency
 parser.add_argument('-0', '--ds0', type=str) # e.g. 2019.07.04
 parser.add_argument('-1', '--ds1', type=str) # e.g. 2019.07.06
-parser.add_argument('-lt', '--list_type', type=str)# hourly, average
+parser.add_argument('-lt', '--list_type', type=str)# hourly, hourly0, average
 # set this to True to interpolate all u, and v fields to the rho-grid
 parser.add_argument('-uv_to_rho', default=True, type=Lfun.boolean_string)
 # Optional: for testing
@@ -51,39 +58,73 @@ parser.add_argument('-test', '--testing', default=False, type=Lfun.boolean_strin
 args = parser.parse_args()
 
 Ldir = Lfun.Lstart()
+
 if args.testing:
     Ldir['local_user'] = 'pmacc'
 
 # name the s3 bucket to look in
 bucket_name = 'liveocean-' + Ldir['local_user']
 
-# create input file list and subsets
+# create start and end day indexes where each item is a start or end day
+# for a given subset
 
-if args.list_type == 'hourly':
-    time_bin_list = pd.date_range(args.ds0,args.ds1,freq='D')
-    for dt in time_bin_list:
-        dt_list = pd.date_range(dt,dt+timedelta(days=1),freq='h')
-        print('')
-        print(dt_list)
+if args.list_type in ['hourly','hourly0']:
+    dt0_ind = pd.date_range(args.ds0,args.ds1,freq='D') # a DatetimeIndex
+    dt1_ind = dt0_ind + timedelta(days=1)
 elif args.list_type == 'average':
-    time_bin_list = pd.date_range(args.ds0,args.ds1,freq='MS')
-    me_list = pd.date_range(args.ds0,args.ds1,freq='ME')
-    bin_dict = dict(zip(time_bin_list, me_list))
-    for dt in time_bin_list:
-        dt_list = pd.date_range(dt,bin_dict[dt],freq='D')
+    dt0_ind = pd.date_range(args.ds0,args.ds1,freq='MS')
+    dt1_ind = pd.date_range(args.ds0,args.ds1,freq='ME')
+dt_dict = dict(zip(dt0_ind, dt1_ind))
+
+# name some things to pass to the subprocess
+gtagex = args.gtagex
+indir = 's3://' + bucket_name + '/LO_roms/' + gtagex
+# NOTE the hard-coded outdir is a problem
+outdir = str(Ldir['LOo']) + '/icechunk_temp'
+list_type = args.list_type
+
+# create a clean directory for the output
+if not args.testing:
+    Lfun.make_dir(outdir, clean=True)
+
+# loop over the subsets
+for dt0 in dt0_ind:
+
+    if (list_type == 'hourly0') and (dt0 > dt0_ind[0]):
+        list_type = 'hourly'
+
+    dt1 = dt_dict[dt0]
+
+    # find the number of jobs to run per subset
+    if list_type == 'hourly0':
+        sub_ind = pd.date_range(dt0,dt1,freq='h')
+    elif list_type == 'hourly':
+        sub_ind = pd.date_range(dt0+timedelta(hours=1),dt1,freq='h')
+    elif list_type == 'average':
+        sub_ind = pd.date_range(dt0,dt1,freq='D')
+    ntimes = len(sub_ind)
+
+    this_ds0 = dt0.strftime(Ldir['ds_fmt'])
+    this_ds1 = dt1.strftime(Ldir['ds_fmt'])
+    cmd_list = ['sbatch','--array=1-'+str(ntimes),
+                './rechunker_worker.sh',
+                indir, outdir, gtagex,
+                this_ds0, this_ds1, list_type]
+    
+    if args.testing:
         print('')
-        print(dt_list)
+        print(' '.join(cmd_list))
+        print(sub_ind)
+    else:
+        proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+        stdout, stderr = proc.communicate()
+        if len(stdout) > 0:
+            print('\nSTDOUT')
+            print(stdout.decode())
+        if len(stderr) > 0:
+            print('\nSTDERR')
+            print(stderr.decode())
 
-
-#print(time_bin_list)
-
-#fn_list0 = Lfun.get_fn_list(Ldir['list_type'], Ldir, Ldir['ds0'], Ldir['ds1'], his_num=1)
-
-# if Ldir['list_type'] == 'hourly':
-
-# elif Ldir['list_type'] == 'average':
-
-# else:
-#     print('unsupported list type')
-#     sys.exit()
+# dt_list = pd.date_range(dt,dt+timedelta(days=1),freq='h')
+# dt_list = pd.date_range(dt,bin_dict[dt],freq='D')
 
--- extract/icechunk/rechunker_worker.sh
+++ extract/icechunk/rechunker_worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-## run using
-# sbatch --array=1-?? ./rechunker_worker.sh cas7_t2_x11b 2026.05.01
+## run using a command of the form
+## sbatch --array=1-?? ./rechunker_worker.sh cas7_t2_x11b 2026.05.01 2026.05.31 average
 
 ## Group
 #SBATCH -A macc
@@ -16,32 +16,34 @@
 #SBATCH --ntasks-per-node=1
 
 ## Walltime 
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 
-## Set memory use. Each slice (32 cores) has 256G.
+## Set memory use. Each slice (32 cores) has 256G, so 8GB per core.
 #SBATCH --mem=8G
 
 # Do not return until the job is finished (use with & on the command line)
 #SBATCH --wait
 
-## Assume some python code uses multi-threading.
-#SBATCH --cpus-per-task=6
-## Note: 25*6 = 150, which is less than the 192 cores we have on a node.
-
+# activate our conda environment
 source /gscratch/macc/parker/miniconda3/etc/profile.d/conda.sh
 conda activate loenv
-# These exports, along with the cpus-per-task set above
-# allow fast multi-threading in python.
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK      # For OpenMP
-export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK # For OpenBLAS
-export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK      # For Intel MKL
-export VECLIB_MAXIMUM_THREADS=$SLURM_CPUS_PER_TASK
-export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# Path to this directory
+# make a unique directory for each task
+export TMPDIR=/var/tmp/$USER/$SLURM_ARRAY_JOB_ID_$SLURM_ARRAY_TASK_ID
+mkdir -p $TMPDIR
+
+# paths
 dir0='/gscratch/macc/parker'
 dir1=${dir0}'/LO/extract/to_zarr'
-# Run the converter
+
+# rename command line arguments for clarity
 gtx=$1
 ds0=$2
-python3 ${dir1}/worker_to_zarr.py -his_str $SLURM_ARRAY_TASK_ID -gtx ${gtx} -ds0 ${ds0} > ${dir1}"/test_"$SLURM_ARRAY_TASK_ID".log"
+ds1=$3
+list_type=$4
+
+# run the worker
+# python3 ${dir1}/rechunker_worker_one_time.py -his_str $SLURM_ARRAY_TASK_ID -gtx ${gtx} -ds0 ${ds0} > ${dir1}"/test_"$SLURM_ARRAY_TASK_ID".log"
+
+# Run the worker
+python3 ${in_dir}/worker.py -tid $SLURM_ARRAY_TASK_ID -gtx ${gtx} -dstr ${dstr} -lt ${lt} -out_dir ${out_dir}
