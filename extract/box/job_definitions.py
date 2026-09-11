f0aebc1544e11821ed834d1d7babac5d3cfea3f9
Parker MacCready <p.maccready@gmail.com>
2026-09-11 08:09:03 -0700

    Update job_definitions.py

--- extract/box/job_definitions.py
+++ extract/box/job_definitions.py
@@ -145,5 +145,5 @@ def get_box(job, Lon, Lat):
         vn_list = 'h,f,pm,pn,mask_rho,salt,temp,zeta,u,v,ubar,vbar,oxygen,NO3,phytoplankton,Uwind,Vwind'
     elif job == 'kelly': # Lydia Kelly and Peter Brodsky (APL)
         aa = [-122.30, -122.58, 47.25, 47.75]
-        vn_list = 'h,f,pm,pn,mask_rho,salt,temp,zeta,u,v'
+        vn_list = 'h,f,pm,pn,mask_rho,salt,temp,zeta,u,v,w'
     return aa, vn_list
