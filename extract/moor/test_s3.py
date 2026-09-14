53fa5ded11399edb46955914f12c375f92af9d50
Parker MacCready <p.maccready@gmail.com>
2026-09-14 15:13:41 -0700

    Update test_s3.py

--- extract/moor/test_s3.py
+++ extract/moor/test_s3.py
@@ -3,15 +3,10 @@ Code to explore mooring-extraction from history files in s3 buckets.
 """
 
 # imports
-import sys, os
-from lo_tools import Lfun, zrfun, zfun
-import argparse
+import os
+from lo_tools import Lfun
 from time import time
-from subprocess import Popen as Po
-from subprocess import PIPE as Pi
-import numpy as np
 import xarray as xr
-
 import s3fs
 
 gtx = 'cas7_t1_x11ab'
@@ -26,14 +21,14 @@ out_dir = Ldir['LOo'] / 'test_s3'
 Lfun.make_dir(out_dir)
 out_fn = out_dir / 'test.nc'
 
-vn_list = 'h,zeta,salt,temp,u,v'
+# vn_list = 'h,zeta,salt,temp,u,v'
 
-cmd_list = ['ncks',
-    '-v', vn_list,
-    '-d', 'xi_rho,'+str(10), '-d', 'eta_rho,'+str(10),
-    '-d', 'xi_u,'+str(10), '-d', 'eta_u,'+str(10),
-    '-d', 'xi_v,'+str(10), '-d', 'eta_v,'+str(10),
-    '--mk_rec_dim','ocean_time']
+# cmd_list = ['ncks',
+#     '-v', vn_list,
+#     '-d', 'xi_rho,'+str(10), '-d', 'eta_rho,'+str(10),
+#     '-d', 'xi_u,'+str(10), '-d', 'eta_u,'+str(10),
+#     '-d', 'xi_v,'+str(10), '-d', 'eta_v,'+str(10),
+#     '--mk_rec_dim','ocean_time']
 
 # fs_s3 = s3fs.S3FileSystem(anon=False)
 
@@ -51,11 +46,13 @@ s3_url = 's3://liveocean-pmacc/LO_roms/' + gtx + '/' + fstr + '/' + fname
 tt0 = time()
 s3_file_obj = fs_s3.open(s3_url, mode='rb',cache_type='blockcache', block_size=2**22)
 print('time for fs_s3.open = %0.1f sec' % (time()-tt0))
+# result: less than 1 sec
 
 # this works
 tt1 = time()
 ds = xr.open_dataset(s3_file_obj, engine='h5netcdf')
 print('time for xr.open_dataset = %0.1f sec' % (time()-tt1))
+# result: less than 3 sec
 
 a = ds.salt[0,:,10,10].values
 print(a)
