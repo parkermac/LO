7867082f75acfae7c368d521ac883893b285514d
Parker MacCready <p.maccready@gmail.com>
2026-09-14 15:18:09 -0700

    Update test_s3.py

--- extract/moor/test_s3.py
+++ extract/moor/test_s3.py
@@ -3,11 +3,16 @@ Code to explore mooring-extraction from history files in s3 buckets.
 """
 
 # imports
+from time import time
+tt00 = time()
+
 import os
 from lo_tools import Lfun
-from time import time
 import xarray as xr
 import s3fs
+
+print('time imports = %0.1f sec' % (time()-tt00))
+
 
 gtx = 'cas7_t1_x11ab'
 fstr = 'f2013.07.02'
@@ -15,22 +20,11 @@ fname = 'ocean_avg_0001.nc'
 
 Ldir = Lfun.Lstart()
 
-fn = Ldir['roms_out'] / gtx / fstr / fname
+#fn = Ldir['roms_out'] / gtx / fstr / fname
 
 out_dir = Ldir['LOo'] / 'test_s3'
 Lfun.make_dir(out_dir)
 out_fn = out_dir / 'test.nc'
-
-# vn_list = 'h,zeta,salt,temp,u,v'
-
-# cmd_list = ['ncks',
-#     '-v', vn_list,
-#     '-d', 'xi_rho,'+str(10), '-d', 'eta_rho,'+str(10),
-#     '-d', 'xi_u,'+str(10), '-d', 'eta_u,'+str(10),
-#     '-d', 'xi_v,'+str(10), '-d', 'eta_v,'+str(10),
-#     '--mk_rec_dim','ocean_time']
-
-# fs_s3 = s3fs.S3FileSystem(anon=False)
 
 ENDPOINT = 'https://s3.kopah.uw.edu'
 fs_s3 = s3fs.S3FileSystem(
@@ -39,8 +33,6 @@ fs_s3 = s3fs.S3FileSystem(
     client_kwargs={'endpoint_url': ENDPOINT},
 )
 
-#s3_url = 'https://s3.kopah.uw.edu/pm-share/kelly_2024.05.01_2024.05.31.nc'
-#s3_url = 's3://pm-share/kelly_2024.05.01_2024.05.31.nc'
 s3_url = 's3://liveocean-pmacc/LO_roms/' + gtx + '/' + fstr + '/' + fname
 
 tt0 = time()
@@ -49,21 +41,12 @@ print('time for fs_s3.open = %0.1f sec' % (time()-tt0))
 # result: less than 1 sec
 
 # this works
-tt1 = time()
+tt0 = time()
 ds = xr.open_dataset(s3_file_obj, engine='h5netcdf')
-print('time for xr.open_dataset = %0.1f sec' % (time()-tt1))
+print('time for xr.open_dataset = %0.1f sec' % (time()-tt0))
 # result: less than 3 sec
 
+tt0 = time()
 a = ds.salt[0,:,10,10].values
 print(a)
-
-# tt_ncks = time()
-# cmd_list += ['-O', str(fn), str(out_fn)]
-# proc = Po(cmd_list, stdout=Pi, stderr=Pi)
-# stdout, stderr = proc.communicate()
-# print(' - time for ncks %0.2f sec' % (time()-tt_ncks))
-
-# # check results
-# ds = xr.open_dataset(out_fn)
-# a = ds.salt.to_numpy().squeeze()
-# print(a)
+print('time to get salt profile = %0.1f sec' % (time()-tt0))
